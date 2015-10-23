5921799e67abc863203a23b915a8c4d55249f94e
Maxwell Bates <max.bates@autodesk.com>
2015-10-23 18:56:46 +0100

    remove lagging Part.js file

---
+++ src/utils/compose.js
@@ -0,0 +1,11 @@
+/**
+ * Composes single-argument functions from right to left.
+ * Could trivially move to multi-var using spread `...arg` if we wanted to...
+ *
+ * @param {...Function} funcs The functions to compose.
+ * @returns {Function} A function obtained by composing functions from right to
+ * left. For example, compose(f, g, h) is identical to arg => f(g(h(arg))).
+ */
+export default function compose(...funcs) {
+  return arg => funcs.reduceRight((composed, f) => f(composed), arg);
+}
