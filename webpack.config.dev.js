b7cb1ba484b9b43c0c23673f2d166f0d81e3f348
Maxwell Bates <max.bates@autodesk.com>
2015-10-28 20:43:35 +0000

    set process.env to dev for dev environment

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,7 +15,12 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('dev')
+      }
+    })
   ],
   module: {
     loaders: [{
