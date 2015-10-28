48e1dc2944a7dbbfb016f47eb22adddeb91dd51c
Deepak Chandran <dchandran1@gmail.com>
2015-10-27 23:45:25 -0700

    Added generic string based schema example and function for converting them to either graphql or validator-based schema

---
+++ src/schemas/schema.js
@@ -0,0 +1,17 @@
+export const Metadata = {
+	version    : 'version',
+	authors    : 'array<id>',
+	tags       : 'hashmap',
+	name       : 'string',
+	description: 'string'
+};
+
+
+export const Annotation = {
+  metadata: Metadata,
+  sequence: 'sequence',
+  start   : 'integer',
+  end     : 'integer'
+};
+
+
