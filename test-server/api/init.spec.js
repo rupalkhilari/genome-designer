750a900558d06182f06531742236e4d6aab74e80
Maxwell Bates <max.bates@autodesk.com>
2015-11-16 10:41:07 -0800

    server in new directory, basic test for server

---
+++ test-server/api/init.spec.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const request = require('supertest');
+
+describe('Server API', () => {
+  let server;
+  beforeEach(function () {
+    server = require('../../devServer').listen();
+  });
+  afterEach(function () {
+    server.close();
+  });
+
+  it('responds to /api/project', function testProject(done) {
+    request(server)
+      .get('/api/project')
+      .expect('yup', done);
+  });
+});
