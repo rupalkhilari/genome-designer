2b30bea4fae6741fc239ca7c6fe0d2d3c0956d22
Deepak Chandran <dhandran1@gmail.com>
2015-12-04 12:11:00 -0800

    a few comments

--- server/api/index.js
+++ server/api/index.js
@@ -213,6 +213,7 @@ router.post('/clone/:id', (req, res) => {
 
 /**
 * File IO
+* Read and write files
 **/
 
 router.get('/file/:url', (req, res) => {
@@ -229,25 +230,29 @@ router.get('/file/:url', (req, res) => {
 router.post('/file/:url', (req, res) => {
   var { url } = req.params;
 
+  //assuming contents to be string
   var buffer = "";
 
+  //All files are put in the storage folder (until platform comes along)
   url = './storage/' + url;
-
   var path = url.substring(0,url.lastIndexOf("/")+1);
 
-  
+  //get data in parts
   req.on('data', data => {
     buffer += data; 
   });
 
+  //received all the data
   req.on('end', function() {
 
+    //make folder if doesn't exists
     mkpath(path, (err) => {
 
       if (err) {
         res.status(500).send(err.message);
       } else {
 
+        //write data to file
         fs.writeFile(url, buffer, (err) => {
 
           if (err) {
