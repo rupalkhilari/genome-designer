c4693febff285859f00f5022ebc38755bfe57ea7
Deepak Chandran <deepak.chandran@autodesk.com>
2015-12-07 17:41:35 -0800

    moved history test case from api_test.py to history.spec.js

--- test/server/REST/history.spec.js
+++ test/server/REST/history.spec.js
@@ -6,8 +6,20 @@ import { set as dbSet } from '../../../server/database';
 const devServer = require('../../../devServer');
 
 describe('REST', () => {
+  
   let server;
   const sessionKey = '123456';
+  const proj1 = {
+         "metadata": {
+            "authors": [],
+            "version": "0.0.0",
+            "tags": {}
+          },
+          "components": [
+          ],
+          "settings": {}
+        };
+
   beforeEach('server setup', () => {
     server = devServer.listen();
     return dbSet(sessionKey, {});
@@ -21,11 +33,59 @@ describe('REST', () => {
   });
 
   describe('History', () => {
-    describe('/ancestors', () => {
+
+    it('logs ancestors and descendants in correct order when cloned repeatedly', (done) => {
+      let pid1,
+          pid2,
+          pid3,
+          pid4;
+
+      request(server).post('/api/project').set('session-key', sessionKey)
+          .send(proj1)
+          .expect(200)
+          .expect((res) => { //proj1 = new project
+            pid1 = res.body.id;
+
+            request(server).post('/api/clone/'+pid1).set('session-key', sessionKey)
+              .expect(200)
+              .expect((res) => { //proj2 = proj1.clone
+                pid2 = res.body.id;
+                request(server).post('/api/clone/'+pid2).set('session-key', sessionKey)
+                  .expect(200)
+                  .expect((res) => {  //proj3 = proj2.clone
+                    pid3 = res.body.id;
+                    request(server).post('/api/clone/'+pid2).set('session-key', sessionKey)
+                      .expect(200)
+                      .expect((res) => {  //proj4 = proj3.clone                        
+                        pid4 = res.body.id;              
+                        request(server).get('/api/ancestors/'+pid4).set('session-key', sessionKey)
+                          .expect(200)
+                          .expect((res) => {  //proj4 = proj3.clone
+                            
+                            //expect parents in correct order
+                            expect(res.body[0]===pid3);
+                            expect(res.body[1]===pid2);
+                            expect(res.body[2]===pid1);
+
+                          });
+
+                        request(server).get('/api/descendants/'+pid1).set('session-key', sessionKey)
+                          .expect(200)
+                          .expect((res) => {  //proj4 = proj3.clone
+                            
+                            //expect parents in correct order
+                            expect(Object.keys(res.body.leaves).length===1);
+                            expect(Object.keys(res.body).length===5);
+
+                          });
+                        
+                      });
+
+                  });               
+              });
+
+          }).end(done);
 
     });
-    describe('/descendants', () => {
-
-    });
-  });
+  });    
 });
