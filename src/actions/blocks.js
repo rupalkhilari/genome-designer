0cc66fc289479074bbeccc762322b41880ea0bb3
Maxwell Bates <max.bates@autodesk.com>
2015-12-07 18:23:51 -0800

    initial stubs for adding sequence to blocks

--- src/actions/blocks.js
+++ src/actions/blocks.js
@@ -20,3 +20,16 @@ export const blockMerge = makeActionCreator(ActionTypes.BLOCK_MERGE, 'blockId', 
 export const blockRename = makeActionCreator(ActionTypes.BLOCK_RENAME, 'blockId', 'name');
 
 export const blockAddComponent = makeActionCreator(ActionTypes.BLOCK_ADD_COMPONENT, 'blockId', 'componentId');
+
+export const blockSetSequence = (blockId, sequence) => {
+  return (dispatch, getState) => {
+    //future - also trigger some history actions
+    const oldBlock = getState().blocks[blockId];
+    //todo - do we want to return the promise? or another action for that? Do we want to follow traditional redux async flow? Where does logic all go?
+    const block = oldBlock.setSequence(sequence);
+    dispatch({
+      type: ActionTypes.BLOCK_SET_SEQUENCE,
+      block,
+    });
+  };
+};
--- src/models/Block.js
+++ src/models/Block.js
@@ -1,5 +1,8 @@
 import Instance from './Instance';
 import randomColor from '../utils/generators/color';
+import { readFile, writeFile } from '../middleware/api';
+
+const sequenceFilePathFromId = (id) => `block/${id}/sequence/`;
 
 export default class Block extends Instance {
   constructor(...args) {
@@ -21,5 +24,43 @@ export default class Block extends Instance {
     const newComponents = this.components.slice();
     newComponents.splice(spliceIndex, 0, component);
     return this.mutate('components', newComponents);
+  }
+
+  /**
+   * @description Retrieve the sequence of the block. Retrieves the sequence from the server, since it is stored in a file, returning a promise.
+   * @param format {String} accepts 'raw', 'fasta', 'genbank'
+   * @returns {Promise} Promise which resolves with the sequence value, or rejects with null if no sequence is associated.
+   */
+  getSequence(format = 'raw') {
+    const hasSequence = typeof this.sequence === 'string' && this.sequence.length;
+    const sequencePath = sequenceFilePathFromId(this.sequence) + `?format=${format}`;
+
+    return hasSequence ?
+      readFile(sequencePath) :
+      Promise.reject(null);
+  }
+
+  /**
+   * @description Set the sequence of the block.
+   *
+   * future - tie in to history
+   *
+   * @param sequence {String} ACTG representation of the sequence. Annotations should be parsed and stored in the block directly in `annotations`.
+   * @returns block {Block} New block with the potentially-updated sequence file path
+   */
+  setSequence(sequence) {
+    //If we are editing the sequence, or sequence doesn't exist, we want to set the sequence for the child block, not change the sequence of the parent part. When setting, it doesn't really matter, we just always want to set via filename which matches this block.
+    const path = sequenceFilePathFromId(this.id);
+
+    //todo - how to handle asynchronous updates like this at model level... Optimistic updates? What about failures? Does this belong in the model? Or separate?
+    writeFile(sequence, path)
+      .then(response => {
+
+      })
+      .catch(error => {
+
+      });
+
+    return this.mutate('sequence', path);
   }
 }
