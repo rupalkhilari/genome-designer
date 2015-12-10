f6fe31b6c5d496d35e4176b15f88b95263a6e6ea
Maxwell Bates <max.bates@autodesk.com>
2015-12-09 16:31:10 -0800

    middleware for get/set blocks address #124

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -1,7 +1,41 @@
 import uuid from 'node-uuid';
 import 'isomorphic-fetch';
+import invariant from '../utils/environment/invariant';
+import BlockDefinition from '../schemas/Block';
+
+/*************************
+ Utils
+ *************************/
 
 const apiPath = (path) => '/api/' + path;
+
+const headersPost = (body) => ({
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body,
+});
+
+const headersDelete = () => ({
+  method: 'DELETE',
+});
+
+/*************************
+ Server Communication
+ *************************/
+
+export const retrieveBlock = (id) => {
+  return fetch(apiPath(`blocks/${id}`));
+};
+
+export const saveBlock = (block) => {
+  invariant(BlockDefinition.validate(block), 'Block does not pass validation: ' + block);
+  const { id } = block;
+  const stringified = JSON.stringify(block);
+
+  return fetch(apiPath(`blocks/${id}`), headersPost(stringified));
+};
 
 export const readFile = (fileName) => {
   return fetch(apiPath(`file/${fileName}`));
@@ -13,16 +47,8 @@ export const writeFile = (fileName = uuid.v4(), contents) => {
   const filePath = apiPath(`file/${fileName}`);
 
   if (contents === null) {
-    return fetch(filePath, {
-      method: 'DELETE',
-    });
+    return fetch(filePath, headersDelete());
   }
 
-  return fetch(filePath, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-    body: contents,
-  });
+  return fetch(filePath, headersPost(contents));
 };
