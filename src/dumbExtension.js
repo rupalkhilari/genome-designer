ecc3c8582f3c6bb219bee88895b1edf4ddf2e3d5
Maxwell Bates <maxbates@gmail.com>
2015-12-11 15:43:53 -0800

    add note on memoization + conversion

--- src/dumbExtension.js
+++ src/dumbExtension.js
@@ -6,7 +6,7 @@ import { render as reactRender } from 'react-dom';
 class SimpleComponent extends React.Component {
   componentWillMount() {
     const self = this;
-    let lastBlockId;
+    let lastBlock;
 
     this.setState({
       block: null,
@@ -17,7 +17,12 @@ class SimpleComponent extends React.Component {
       const { currentInstance } = store.ui;
       const block = !!currentInstance ? store.blocks[currentInstance] : null;
 
-      if (block.id !== lastBlockId) {
+      // all instances in the store are immutables, so you can just do a reference equality check to see if it has changed
+      // this would also be a good place to convert to the onion format
+      // it would be a great place to memoize
+      // (i.e. return same value if input === previous input, rather than storing it explicitly in the component,
+      // and then you can reuse the selector for all things you retrieve from the store)
+      if (block !== lastBlock) {
         //note that right now, blocks dont have a sequence... this will work but nothing will be returned
         block.getSequence()
           .then(sequence => {
@@ -26,7 +31,7 @@ class SimpleComponent extends React.Component {
               sequence,
             });
           });
-        lastBlockId = block.id;
+        lastBlock = block;
       }
     };
 
