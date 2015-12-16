529431be7ca85b5ad5cdc105125f058c5dbea7f3
Maxwell Bates <max.bates@autodesk.com>
2015-12-16 14:37:41 -0800

    add middleware tests for read/ write files close #132

--- test/middleware/api.js
+++ test/middleware/api.js
@@ -1,9 +1,11 @@
 import chai from 'chai';
-import { apiPath, getSessionKey } from '../../src/middleware/api';
-
+import fs from 'fs';
+import { apiPath, getSessionKey, writeFile, readFile } from '../../src/middleware/api';
 const { expect } = chai;
 
-describe('Middleware', () => {
+const fileStoragePath = './storage/';
+
+describe.only('Middleware', () => {
   //login() is tested in server/REST
 
   it('apiPath() returns an absolute URL to hit the server', () => {
@@ -26,11 +28,91 @@ describe('Middleware', () => {
 
   //todo
 
-  it('writeFile() should take path and string, and write file');
+  it('writeFile() should take path and string, and write file', (done) => {
+    const filePath = 'test/writeMe';
+    const fileContents = 'rawr';
+    const storagePath = fileStoragePath + filePath;
 
-  it('writeFile() shuold delete if contents are null');
+    return writeFile(filePath, fileContents)
+      .then((res) => {
+        expect(res.status).to.equal(200);
+        fs.readFile(storagePath, 'utf8', (err, file) => {
+          expect(err).to.eql(null);
+          expect(file).to.eql(fileContents);
+          done();
+        });
+      });
+  });
 
-  it('readFile() should return fetch response object');
+  it('writeFile() shuold delete if contents are null', (done) => {
+    const filePath = 'test/deletable';
+    const fileContents = 'oopsie';
+    const storagePath = fileStoragePath + filePath;
 
-  it('should work with multiple files');
+    fs.writeFile(storagePath, fileContents, 'utf8', (err, write) => {
+      writeFile(filePath, null).then((res) => {
+        expect(res.status).to.equal(200);
+        fs.readFile(storagePath, 'utf8', (err, read) => {
+          expect(err.code).to.equal('ENOENT');
+          done();
+        });
+      });
+    });
+  });
+
+  it('readFile() should return fetch response object', (done) => {
+    const filePath = 'test/readable';
+    const fileContents = 'the contents!';
+    const storagePath = fileStoragePath + filePath;
+
+    fs.writeFile(storagePath, fileContents, 'utf8', (err, write) => {
+      readFile(filePath)
+        .then(result => {
+          expect(result.status).to.equal(200);
+          expect(typeof result.text).to.equal('function');
+          return result.text();
+        })
+        .then(text => {
+          expect(text).to.equal(fileContents);
+          done();
+        });
+    });
+  });
+
+  it('should work with multiple files', function multipleFiles(done) {
+    this.timeout(5000);
+
+    const file1Path = 'test/file1';
+    const file1Contents = 'exhibit a';
+    const storage1Path = fileStoragePath + file1Path;
+
+    const file2Path = 'test/file2';
+    const file2Contents = 'exhibit b';
+    const storage2Path = fileStoragePath + file2Path;
+
+    Promise
+      .all([
+        writeFile(file1Path, file1Contents),
+        writeFile(file2Path, file2Contents),
+      ])
+      .then(files => {
+        fs.readFile(storage1Path, 'utf8', (err, read) => {
+          expect(read).to.equal(file1Contents);
+        });
+        fs.readFile(storage2Path, 'utf8', (err, read) => {
+          expect(read).to.equal(file2Contents);
+        });
+      })
+      .then(() => {
+        return Promise.all([
+          readFile(file1Path).then(resp => resp.text()),
+          readFile(file2Path).then(resp => resp.text()),
+        ]);
+      })
+    .then(files => {
+      expect(files[0]).to.equal(file1Contents);
+      expect(files[1]).to.equal(file2Contents);
+      done();
+    });
+  });
 });
