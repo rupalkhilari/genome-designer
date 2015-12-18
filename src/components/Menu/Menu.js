fe58c2c948a27195ba0b45d5b81aab3145816747
Maxwell Bates <max.bates@autodesk.com>
2015-12-17 16:24:08 -0800

    simplify menu commands. strip out mouseout behavior

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,87 +1,39 @@
 import React, { Component, PropTypes } from 'react';
 
-/**
- * Popup window class. Accepts any component as it client.
- * Required properties:
- *
- * {String} title - title bar text for window
- * {Function} onClose - function to call when the window is closed
- * {ReactElement} client - element to place in the client area
- */
+import MenuItem from './MenuItem';
+import MenuSeparator from './MenuSeparator';
+
 export default class Menu extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false
-    }
-  }
-
   static propTypes = {
-    title         : PropTypes.string.isRequired,
-    menuItems     : PropTypes.array.isRequired,
-    parentMenuBar : PropTypes.object.isRequired
+    isOpen: PropTypes.bool.isRequired,
+    onToggle: PropTypes.func.isRequired,
+    title: PropTypes.string.isRequired,
+    menuItems: PropTypes.array.isRequired,
   }
 
-  /**
-   * change state to open and tell our parent menu bar that we are open
-   */
-  open = () => {
-    this.props.parentMenuBar.menuOpening(this);
-    this.setState({
-      isOpen: true
-    });
+  toggle = (forceVal) => {
+    this.props.onToggle(forceVal);
   }
 
-  /**
-   * change state to closed
-   */
-  close = () => {
-    this.props.parentMenuBar.menuClosing(this);
-    this.setState({
-      isOpen: false
-    });
-  }
-
-  /**
-   * toggle open state
-   */
-  toggle = () => {
-    if (this.state.isOpen) {
-      this.close();
-    } else {
-      this.open();
-    }
-  }
-
-  /**
-   * mouse over the drop down shows the menu IF there is already a menu open.
-   * Only our parent menu bar can perform this functionality
-   */
-  mouseOver = () => {
-    this.props.parentMenuBar.mouseOverMenu(this);
-  }
-
-  /**
-   * render menu
-   * @return {<Menu>}
-   */
   render() {
-
-    // container and associated child menu items is only rendered when open
-    let container = this.state.isOpen ?
-    <div className="menu-dropdown-container">
-      {this.props.menuItems}
-    </div> : null;
-
-    // prepare class to reflect open / closed state
-    var klassName = this.state.isOpen ? "menu-header menu-header-open" : "menu-header";
-
-    // render the menu drop down and header and optionally the child items
     return (
-      <div className="menu-dropdown" onClick={this.toggle} onMouseOver={this.mouseOver}>
-        <div className={klassName} onClick={this.toggle}>{this.props.title}</div>
-        {container}
+      <div className="menu-dropdown">
+        <div className={this.props.isOpen ? 'menu-header menu-header-open' : 'menu-header'}
+             onClick={this.toggle}>
+          {this.props.title}
+        </div>
+        {this.props.isOpen && (
+          <div className="menu-dropdown-container">
+            {this.props.menuItems.map(item => {
+              return (
+                item.text ?
+                  (<MenuItem text={item.text} action={item.action}/>) :
+                  (<MenuSeparator />)
+                );
+            })}
+          </div>
+        )}
       </div>
     );
   }
--- src/containers/GlobalNav.js
+++ src/containers/GlobalNav.js
@@ -2,174 +2,95 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { pushState } from 'redux-router';
-import { projectCreate } from '../actions/projects';
-import { blockCreate } from '../actions/blocks';
-import { projectAddConstruct } from '../actions/projects';
-import { inventoryToggleVisibility } from '../actions/inventory';
-
 
 import MenuBar from '../components/Menu/MenuBar';
-import Menu from '../components/Menu/Menu';
-import MenuItem from '../components/Menu/MenuItem';
-import MenuSeparator from '../components/Menu/MenuSeparator';
-
 
 import '../styles/GlobalNav.css';
 
 class GlobalNav extends Component {
   static propTypes = {
-    projects: PropTypes.object.isRequired,
-    inputValue: PropTypes.string.isRequired,
-    projectCreate: PropTypes.func.isRequired,
     pushState: PropTypes.func.isRequired,
-    blockCreate: PropTypes.func.isRequired,
     currentProjectId: PropTypes.string,
-    projectAddConstruct: PropTypes.func.isRequired,
-    inventoryToggleVisibility: PropTypes.func.isRequired,
   }
 
   state = {
     showAddProject: false,
   }
 
-  handleClickInventory = (event) => {
-    this.props.inventoryToggleVisibility();
-  }
-
-  handleClickAddProject = (event) => {
-    const { pushState, projectCreate } = this.props;
-    projectCreate().then(project => {
-      pushState(null, `/project/${project.id}`);
-    });
-  }
-
-  handleClickAddConstruct = (event) => {
-    const { currentProjectId, blockCreate, projectAddConstruct } = this.props;
-    blockCreate()
-      .then(block => {
-        projectAddConstruct(currentProjectId, block.id);
-      });
-  }
-
-  /**
-   * generic unhandled menu item
-   * @return {[type]} [description]
-   */
-  onMenuItem = () => {
-
-  }
-
   render() {
-    const { currentProjectId } = this.props;
-
     return (
       <div className="GlobalNav">
         <Link className="GlobalNav-title"
               to="/">GD</Link>
         <MenuBar menus={[
-          <Menu title="FILE" menuItems={[
-            <MenuItem text="Recent Projects" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="New Project" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="New Construct" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="New Construct from Clipboard" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="New Instance" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Invite Collaborators" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Upload Genbank File" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Download Genbank File" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Export PDF" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Publish to Gallery" onClick={this.onMenuItem}></MenuItem>,
-          ]}></Menu>,
-
-          <Menu title="EDIT" menuItems={[
-            <MenuItem text="Undo" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Redo" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Cut" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Copy" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Copy As..." onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Paste" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Rename" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Duplicate" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Delete" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Crop Sequence to Selection" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Convert to List" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Convert to Construct" onClick={this.onMenuItem}></MenuItem>,
-          ]}></Menu>,
-
-          <Menu title="VIEW" menuItems={[
-            <MenuItem text="Inventory" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Inspector" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Toolbar" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="History" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Sequence" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Hide/Show Annotations" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Hide/Show List Contents" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Compare..." onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Labels Only" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Symbols Only" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Labels + Symbols" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Custom" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Full Width" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Compact" onClick={this.onMenuItem}></MenuItem>,
-            <MenuItem text="Wrap" onClick={this.onMenuItem}></MenuItem>,
-            <MenuSeparator/>,
-            <MenuItem text="Preview Deletions" onClick={this.onMenuItem}></MenuItem>,
-          ]}></Menu>,
-
-        <Menu title="HELP" menuItems={[
-          <MenuItem text="User Guide" onClick={this.onMenuItem}></MenuItem>,
-          <MenuItem text="Show Tutorial" onClick={this.onMenuItem}></MenuItem>,
-          <MenuItem text="Keyboard Shortcuts" onClick={this.onMenuItem}></MenuItem>,
-          <MenuItem text="Community Forum" onClick={this.onMenuItem}></MenuItem>,
-          <MenuItem text="Get Support" onClick={this.onMenuItem}></MenuItem>,
-          <MenuItem text="Give Us Feedback" onClick={this.onMenuItem}></MenuItem>,
-          <MenuSeparator/>,
-          <MenuItem text="About Genome Designer" onClick={this.onMenuItem}></MenuItem>,
-          <MenuItem text="Terms of Use" onClick={this.onMenuItem}></MenuItem>,
-          <MenuItem text="Privacy Policy" onClick={this.onMenuItem}></MenuItem>,
-          ]}></Menu>,
-
-        ]}></MenuBar>
-      </div>
-    );
-  }
-
-  renderXXX() {
-    const { currentProjectId } = this.props;
-
-    return (
-      <div className="GlobalNav">
-        <Link className="GlobalNav-title"
-              to="/">GD</Link>
-        <a className="GlobalNav-action"
-           disabled={!currentProjectId}
-           onClick={this.handleClickInventory}>
-          Inventory
-        </a>
-        <a className={'GlobalNav-action'}
-           onClick={this.handleClickAddProject}>
-          Add Project
-        </a>
-        <a className={'GlobalNav-action'}
-           disabled={!currentProjectId}
-           onClick={this.handleClickAddConstruct}>
-          Add Construct
-        </a>
-        <a className={'GlobalNav-action'}
-           disabled //todo - check for current construct / block being selected
-           onClick={this.handleClickAddConstruct}>
-          Add Block
-        </a>
-
+          {text: 'FILE', items: [
+             { text: 'Recent Projects', action: () => {} },
+             {},
+             { text: 'New Project', action: () => {} },
+             { text: 'New Construct', action: () => {} },
+             { text: 'New Construct from Clipboard', action: () => {} },
+             { text: 'New Instance', action: () => {} },
+             {},
+             { text: 'Invite Collaborators', action: () => {} },
+             { text: 'Upload Genbank File', action: () => {} },
+             { text: 'Download Genbank File', action: () => {} },
+             { text: 'Export PDF', action: () => {} },
+             {},
+             { text: 'Publish to Gallery', action: () => {} },
+          ]},
+          {text: 'EDIT', items: [
+             { text: 'Undo', action: () => {} },
+             { text: 'Redo', action: () => {} },
+             {},
+             { text: 'Cut', action: () => {} },
+             { text: 'Copy', action: () => {} },
+             { text: 'Copy As...', action: () => {} },
+             { text: 'Paste', action: () => {} },
+             {},
+             { text: 'Rename', action: () => {} },
+             { text: 'Duplicate', action: () => {} },
+             { text: 'Delete', action: () => {} },
+             {},
+             { text: 'Crop Sequence to Selection', action: () => {} },
+             {},
+             { text: 'Convert to List', action: () => {} },
+             { text: 'Convert to Construct', action: () => {} },
+          ]},
+          {text: 'VIEW', items: [
+             { text: 'Inventory', action: () => {} },
+             { text: 'Inspector', action: () => {} },
+             { text: 'Toolbar', action: () => {} },
+             { text: 'History', action: () => {} },
+             { text: 'Sequence', action: () => {} },
+             {},
+             { text: 'Hide/Show Annotations', action: () => {} },
+             { text: 'Hide/Show List Contents', action: () => {} },
+             { text: 'Compare...', action: () => {} },
+             {},
+             { text: 'Labels Only', action: () => {} },
+             { text: 'Symbols Only', action: () => {} },
+             { text: 'Labels + Symbols', action: () => {} },
+             { text: 'Custom', action: () => {} },
+             {},
+             { text: 'Full Width', action: () => {} },
+             { text: 'Compact', action: () => {} },
+             { text: 'Wrap', action: () => {} },
+             {},
+             { text: 'Preview Deletions', action: () => {} },
+          ]},
+        {text: 'HELP', items: [
+           { text: 'User Guide', action: () => {} },
+           { text: 'Show Tutorial', action: () => {} },
+           { text: 'Keyboard Shortcuts', action: () => {} },
+           { text: 'Community Forum', action: () => {} },
+           { text: 'Get Support', action: () => {} },
+           { text: 'Give Us Feedback', action: () => {} },
+           {},
+           { text: 'About Genome Designer', action: () => {} },
+           { text: 'Terms of Use', action: () => {} },
+           { text: 'Privacy Policy', action: () => {} },
+        ]},
+      ]}/>
       </div>
     );
   }
@@ -178,15 +99,9 @@ class GlobalNav extends Component {
 function mapStateToProps(state) {
   return {
     currentProjectId: state.router.params.projectId,
-    projects: state.projects,
-    inputValue: state.router.params.projectId || '',
   };
 }
 
 export default connect(mapStateToProps, {
   pushState,
-  blockCreate,
-  projectCreate,
-  projectAddConstruct,
-  inventoryToggleVisibility,
 })(GlobalNav);
