4dff254654409e983131e5859c704fba4782dfa0
Duncan Meech <duncanmeech@gmail.com>
2016-01-12 09:06:16 -0800

    use execute block to count blocks in project and verify dnd increases it

--- test-e2e/tests/draganddrop.js
+++ test-e2e/tests/draganddrop.js
@@ -15,15 +15,41 @@ module.exports = {
       .waitForElementPresent('.SidePanel.Inspector.visible', 5000, 'Expected inspector to be visible')
       // expect at least one inventory item and one block to drop on
       .waitForElementPresent('.InventoryItem-item', 5000, 'expected an inventory item')
-      .waitForElementPresent('[data-testblock]', 5000, 'expected a block to drop on')
-      // click and drag first inventory item
+      .waitForElementPresent('[data-testblock]', 5000, 'expected a block to drop on');
+
+    // count the number of blocks and sbol blocks
+    var blocks, sbolBlocks, total;
+    browser.execute(function() {
+      return {
+        blocks: document.querySelectorAll('[data-testblock]').length,
+        sbol: document.querySelectorAll('[data-testsbol]').length,
+      }
+    }, [], function(result) {
+      blocks = result.value.blocks;
+      sbolBlocks = result.value.sbol;
+      total = blocks + sbolBlocks;
+      browser.assert.ok(total === 5, 'expect there to be 5 blocks in the project');
+    });
+
+    // click and drag first inventory item
+    browser
       .moveToElement('.InventoryItem-item', 10, 10)
       .mouseButtonDown(0)
-      .pause(500)
       .moveToElement('[data-testblock]', 10, 10)
-      .pause(500)
-      .mouseButtonUp(0)
-      .pause(10000)
-      .end();
+      .mouseButtonUp(0);
+
+    // verify the number of blocks increased
+    browser.execute(function() {
+      return {
+        blocks: document.querySelectorAll('[data-testblock]').length,
+        sbol: document.querySelectorAll('[data-testsbol]').length,
+      }
+    }, [], function(result) {
+      blocks = result.value.blocks;
+      sbolBlocks = result.value.sbol;
+      browser.assert.ok(blocks + sbolBlocks === total + 1, 'expect there to be 1 more block in the project');
+    });
+
+    browser.end();
   }
 };
