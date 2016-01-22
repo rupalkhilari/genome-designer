cab66cd1a1e32e9a7393ddb076cc2672d4f83f2f
Maxwell Bates <max.bates@autodesk.com>
2016-01-21 17:29:42 -0800

    initial library updates, not yet working

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -27,7 +27,7 @@ module.exports = Object.assign({}, webpackBase, {
     loaders: [
       {
         test   : /\.js$/,
-        loaders: ['babel'],
+        loader: 'babel-loader',
         include: path.join(__dirname, 'src'),
         exclude: /node_modules/
       },
