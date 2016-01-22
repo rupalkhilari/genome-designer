9801365f43d11e7b7a1d09414c0ae4d1c3d79aed
Maxwell Bates <max.bates@autodesk.com>
2016-01-21 16:14:35 -0800

    add selectors for each section of store

--- src/selectors/blocks.js
+++ src/selectors/blocks.js
@@ -2,40 +2,59 @@
  * Parent accessing / store knowledge-requiring
  ***************************************/
 
-//todo - work for project.components
-const getParentFromStore = (blockId, store, def) => {
+const _getBlockFromStore = (blockId, store) => {
+  return store.blocks[blockId];
+};
+
+const _getParentFromStore = (blockId, store, def) => {
   const id = Object.keys(store.blocks).find(id => {
-    return store.blocks[id].components.includes(blockId);
+    return _getBlockFromStore(id, store).components.includes(blockId);
   });
   return !!id ? store.blocks[id] : def;
 };
 
-//Non-mutating
+export const blockGet = (blockId) => {
+  return (dispatch, getState) => {
+    return _getBlockFromStore(blockId, getState());
+  };
+};
+
 export const blockGetParents = (blockId) => {
   return (dispatch, getState) => {
     const parents = [];
     const store = getState();
-    let parent = getParentFromStore(blockId, store);
+    let parent = _getParentFromStore(blockId, store);
     while (parent) {
       parents.push(parent.id);
-      parent = getParentFromStore(parent.id, store);
+      parent = _getParentFromStore(parent.id, store);
     }
     return parents;
   };
 };
 
-//Non-mutating
+export const blockGetChildrenRecursive = (blockId, childId) => {
+  return (dispatch, getState) => {
+    //const children = [];
+    const store = getState();
+    const block = _getBlockFromStore(blockId);
+
+    const getChildrenShallow = (block) => block.components.map(id => _getBlockFromStore(id, store));
+
+    //todo - recursive
+    return getChildrenShallow(block);
+  };
+};
+
 export const blockGetSiblings = (blockId) => {
   return (dispatch, getState) => {
-    const parent = getParentFromStore(blockId, getState(), {});
+    const parent = _getParentFromStore(blockId, getState(), {});
     return parent.components;
   };
 };
 
-//Non-mutating
 export const blockGetIndex = (blockId) => {
   return (dispatch, getState) => {
-    const parent = getParentFromStore(blockId, getState(), {});
+    const parent = _getParentFromStore(blockId, getState(), {});
     return Array.isArray(parent.components) ? parent.components.indexOf(blockId) : -1;
   };
 };
