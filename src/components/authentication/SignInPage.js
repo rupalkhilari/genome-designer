8d2eed7b1ac21b1ab3dfc07c95d2b0269132ca03
Duncan Meech <duncanmeech@gmail.com>
2016-01-29 12:02:35 -0800

    add all remaining authentication forms

--- src/components/authentication/SignInPage.js
+++ src/components/authentication/SignInPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import LoginForm from './signin.js';
 import RegisterForm from './register.js';
+import ResetPasswordForm from './resetpassword.js';
+import UpdateCredentialsForm from './updatecredentials.js';
+import ForgotPasswordForm from './forgot.js';
 
 import '../../../src/styles/Authentication.css';
 
@@ -9,6 +12,9 @@ export default function LoginPage(props) {
     <div style={{margin: "10rem auto"}}>
       <LoginForm/>
       <RegisterForm/>
+      <ResetPasswordForm/>
+      <UpdateCredentialsForm/>
+      <ForgotPasswordForm/>
     </div>
   );
 }
