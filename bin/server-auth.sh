09a731a15cee65d4dedb730b6a5a3e2e11fcf752
dhylbert <hylbert@gmail.com>
2016-02-11 10:53:32 -0800

    work around npm installing same version of npm modules from git URLs

--- bin/server-auth.sh
+++ bin/server-auth.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+VERSION_FILE="./node_modules/bio-user-platform/package.json"
+VERSION="0.0.1"
+
 correct_cwd () {
     if [ ! -f "package.json" ]
     then
@@ -15,13 +18,26 @@ correct_cwd () {
     fi
 }
 
+install_platform () {
+    if [ -f "$VERSION_FILE" ]
+    then
+        CURRENT_VERSION=$(grep '"version":' ${VERSION_FILE} | tr -d ' ' | tr -d ',' | tr -d '"' | cut -f 2 -d :)
+        if [ "$CURRENT_VERSION" == "$VERSION" ]
+        then
+            return 0
+        fi
+    fi
+    npm install git+https://git.autodesk.com:bionano/bio-user-platform.git#v${VERSION}
+}
+
 correct_cwd
+install_platform
+
 TARGET="npm start"
 if [ "$COMMAND" != "" ]
 then
-    TARGET=$COMMAND
+    TARGET=${COMMAND}
 fi
 
 echo "executing $TARGET with authentication enabled..."
-npm install git+https://git.autodesk.com:bionano/bio-user-platform.git#v0.0.1
-BIO_NANO_AUTH=1 $TARGET
+BIO_NANO_AUTH=1 ${TARGET}
