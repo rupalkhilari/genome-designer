4363c97025bc50deadce8a857452e836a63b4e99
Maxwell Bates <max.bates@autodesk.com>
2016-03-09 11:11:40 -0800

    add notes to genbank index

--- plugins/convert/genbank/index.js
+++ plugins/convert/genbank/index.js
@@ -3,6 +3,115 @@ import * as fileSystem from '../../../server/utils/fileSystem';
 import path from 'path';
 import Project from '../../../src/models/Project';
 import Block from '../../../src/models/Block';
+import merge from 'lodash.merge'
+import BlockDefinition from '../../../src/schemas/Block';
+import ProjectDefinition from '../../../src/schemas/Project';
+import md5 from 'md5';
+import * as persistence from '../../../server/data/persistence';
+
+//EXAMPLE
+
+// you likely will call RunCommand() which will run the python, which writes a file and then reads it.
+// The Python should not be responsible for correct data structure - take out scaffolds from that,
+// and just return JSON objects in format which makes sense for capturing and returning the data available in Genbank files
+// Pass the output to a script like this, which will scaffold + massage it for you, and write the sequence files.
+// Then, pass the output of this section (a promise, which resolves to a `rollup`).
+
+//from python, incomplete definitions:
+
+//whatever project data you want to export.... I just have these fields so that the handling below is consistent
+const outputProject = {
+  name: 'blah blah',
+  description: 'this is a description',
+};
+
+//array of output blocks in whatever format you choose
+const outputBlocks = [
+  {
+    name: 'name',
+    sequence: 'acggatcga',
+    annotations: [],
+  },
+  //etc
+];
+
+//data massating
+const handleBlocks = (outputBlocks) => {
+  return Promise.all(
+    outputBlocks.map(outputBlock => {
+      // generate a valid block scaffold. This is similar to calling new Block(),
+      // but a bit more light weight and easier to work with (models are frozen so you cannot edit them)
+      const scaffold = BlockDefinition.scaffold();
+
+      //get the sequence md5
+      const sequenceMd5 = outputBlock.sequence ? md5(outputBlock.sequence) : '';
+
+      //reassign values
+      const toMerge = {
+        metadata: {
+          name: outputBlock.name,
+        },
+        sequence: {
+          md5: sequenceMd5,
+          annotations: outputBlock.annotations, //you will likely have to remap these...
+        },
+        source: {
+          id: 'genbank',
+        },
+      };
+
+      //be sure to pass in empty project first, so you arent overwriting scaffold each time
+      const block = merge({}, scaffold, toMerge);
+
+      //if you want to validate it afterwards (probably for debugging)
+      //This function will log errors about what fields failed validation
+      //you can pass in a second argument of true for this to throw if you want a stack trace
+      const isValid = BlockDefinition.validate(block);
+
+      //note that if you reject this promise, then Promise.all will reject as well. You probably dont want to do that.
+      //None of the blocks will be available on what is returned. Dont want you to run into that.
+      //Really, you probably just dont want this to happen ever...
+      if (!isValid) {
+        //return Promise.reject('block was not valid:', block);
+      }
+
+      //promise, for writing sequence if we have one, or just immediately resolve if we dont
+      const sequencePromise = sequenceMd5 ?
+        persistence.sequenceWrite(sequenceMd5, outputBlock.sequence) :
+        Promise.resolve();
+
+      //return promise which will resolve with block once done
+      return sequencePromise.then(() => block);
+    })
+  );
+};
+
+const handleProject = (outputProject) => {
+  //just get fields we want using destructuring and use them to merge
+  const { name, description } = outputProject;
+
+  return merge({}, ProjectDefinition.scaffold(), {
+    metadata: {
+      name,
+      description,
+    },
+  });
+};
+
+/*
+ NOTES
+ ______
+
+ -- You probably dont need to touch file writing and reading too much, but if you do, you should look at fileSystem.js and filePaths.js
+ They should be fairly self-explanatory, with examples available here and elsewhere.
+
+ -- I would recommend not using the field ID, or not assigning it. Let the scaffold handle that.
+
+ -- You can see what the schema consists of by calling describe() on the schema:
+ console.log(BlockDefinition.describe());
+
+ -- Show/Hide Store in the running app with Ctrl-h (and move it with ctrl-q)
+ */
 
 const exec = require('child_process').exec;
 const uuid = require('node-uuid');
@@ -55,6 +164,7 @@ export const importProject = (genbankString) => {
 };
 
 //todo - this is a weird syntax of {block, blocks} and should be made consistent with `rollups` or at the very least better explained why exception is made
+//see format of rollup in /server/data/README.md
 
 export const exportBlock = (block, blocks) => {
   const proj = new Project();
@@ -69,7 +179,7 @@ export const importBlock = (gbstr) => {
         result.project.components && result.project.components.length > 0) {
         const bid = result.project.components[0];
         const block = result.blocks[bid];
-        return Promise.resolve({block: block, blocks: result.blocks});
+        return Promise.resolve({ block: block, blocks: result.blocks });
       }
       return Promise.reject('invalid input string');
     });
