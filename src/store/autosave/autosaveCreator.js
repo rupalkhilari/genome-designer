05458321b3dfb097c290b59b393ed057bb711dcf
Maxwell Bates <max.bates@autodesk.com>
2016-04-15 14:08:45 -0700

    smarter autosaving. can forceOn actions. shorter times. better debouncing / throttling. better purging (not on save, wasnt throttling)

--- src/store/autosave/autosaveCreator.js
+++ src/store/autosave/autosaveCreator.js
@@ -2,17 +2,16 @@ import invariant from 'invariant';
 import { debounce, throttle } from 'lodash';
 import { FORCE_SAVE } from './ActionTypes';
 
-//todo - avoid triggering change if initiated by onSave
-
 export default function autosavingCreator(config) {
   invariant(typeof config.onSave === 'function', 'must pass onSave to autosaving middleware');
 
   const options = Object.assign({
-    time: 30 * 1000, //throttle autosave requests, 30 sec
-    wait: 5 * 1000, //debounce wait time, 5 sec
-    filter: () => true, //in addition to state change, pass a filter for triggering autosave events
-    purgeOn: () => false, //will cancel throttled calls if pass function
-    onSave: () => {},
+    time: 20 * 1000, //throttle autosave requests, 20 sec
+    wait: 3 * 1000, //debounce wait time, 2.5 sec
+    onSave: (nextState) => { throw new Error('onSave is required'); },
+    filter: (action, alreadyDirty, nextState, lastState) => nextState !== lastState, //filter for triggering autosave events
+    purgeOn: (action, alreadyDirty, nextState, lastState) => false, //will cancel throttled calls if pass function
+    forceOn: (action, alreadyDirty) => false, //force save on certain actions (so not debounced)
     forceSaveActionType: FORCE_SAVE,
   }, config);
 
@@ -23,6 +22,7 @@ export default function autosavingCreator(config) {
   const isDirty = () => dirty;
 
   const handleSave = (nextState) => {
+    console.log('saving');
     lastSaved = +Date.now();
     options.onSave(nextState);
     dirty = false;
@@ -34,39 +34,52 @@ export default function autosavingCreator(config) {
     trailing: true,
   });
 
+  //want to initiate saves debounced
+  //not check saves debounced
+  const debouncedInitiateSave = debounce(throttledSave, options.wait, {
+    leading: false,
+    trailing: true,
+    maxWait: options.time,
+  });
+
   const checkSave = (action, nextState, lastState) => {
-    if (lastState !== nextState && options.filter(action, nextState, lastState)) {
+    if (options.filter(action, dirty, nextState, lastState) === true) {
       dirty = true;
     }
     return dirty;
   };
 
-  //if we want to debounce checking whether to save
-  const checkSaveDebounced = debounce(checkSave, options.wait, {
-    leading: false,
-    maxWait: 15 * 1000,
-  });
+  const purgeDebounced = () => {
+    throttledSave.cancel();
+    debouncedInitiateSave.cancel();
+  };
+
+  const handleForceSave = (state) => {
+    purgeDebounced();
+    handleSave(state);
+    return state;
+  };
 
   const autosaveReducerEnhancer = reducer => {
     //track for reducer initial state because if the reducer e.g. use default prop of an object, reference will be different between initial state we compute here and when the reducer computes initial state as normal, triggering save on store initialization
     let initialized = false;
 
     return (state, action) => {
-      if (action.type === options.forceSaveActionType) {
-        throttledSave.cancel();
-        handleSave(state);
+      //intercept action, dont need to run reducer
+      if (action.type === options.forceSaveActionType || options.forceOn(action, dirty) === true) {
+        handleForceSave(state);
         return state;
       }
 
       const nextState = reducer(state, action);
 
-      if (options.purgeOn(action, nextState, state)) {
-        throttledSave.cancel();
+      if (options.purgeOn(action, dirty, nextState, state) === true) {
+        purgeDebounced();
       }
 
       //function call so easy to transition to debounced version
       if (checkSave(action, nextState, state) && !!initialized) {
-        throttledSave(nextState);
+        debouncedInitiateSave(nextState);
       }
 
       initialized = true;
--- src/store/autosave/autosaveInstance.js
+++ src/store/autosave/autosaveInstance.js
@@ -3,22 +3,32 @@ import * as ActionTypes from '../../constants/ActionTypes';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
 const purgeEvents = [
-  ActionTypes.PROJECT_SAVE,
+  //ActionTypes.PROJECT_SAVE, //dont purge on this, since call it on save, will never throttle
   ActionTypes.PROJECT_SNAPSHOT,
-  LOCATION_CHANGE,
+  //LOCATION_CHANGE, //forceOn will handle purging for us, do not needed here
 ];
 
 const autosave = autosaveCreator({
   //filter to undoable actions, which basically are the ones that are state changes (undoable reducer relies on these)
-  filter: (action, nextState, lastState) => !!action.undoable,
+  filter: (action, alreadyDirty, nextState, lastState) => !!action.undoable,
+
+  //also, force on location change
+  //want this to run prior to route change -- note if have other middleware to prune store
+  //only run if this reducer was updated so compare states
+  //if dont compare states, will likely trigger on all route changes (including init, which will not find window.gd.api
+  forceOn: ({ type }, alreadyDirty) => {
+    return type === LOCATION_CHANGE && alreadyDirty;
+  },
 
   //this is pretty hack, but want to rely on action to do this (and actions have a dependency on the store, so cant import directly or create circular dependency. just need to be sure this doesnt run until after everything has been set up...
   onSave: () => {
-    console.log('saving!');
+    console.log('onSave called!');
     window.gd.api.projects.projectSave();
   },
 
-  purgeOn: ({ type }, nextState, lastState) => purgeEvents.some(eventType => eventType === type),
+  purgeOn: ({ type }, alreadyDirty, nextState, lastState) => {
+    return purgeEvents.some(eventType => eventType === type);
+  },
 });
 
 export default autosave;
