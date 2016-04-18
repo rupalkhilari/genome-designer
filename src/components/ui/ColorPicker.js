3dc1cd95c8433888cd4c77de30635e9d4939083d
Maxwell Bates <max.bates@autodesk.com>
2016-04-18 13:30:49 -0700

    initial refactor color/symbol picker. add noSymbol.svg. style changes

--- src/components/ui/ColorPicker.js
+++ src/components/ui/ColorPicker.js
@@ -1,28 +1,79 @@
-import React, { PropTypes } from 'react';
-import { colors } from '../../utils/generators/color';
+import React, { Component, PropTypes } from 'react';
+import { colorMap, colors } from '../../utils/generators/color';
+import PickerItem from './PickerItem';
 
 import '../../styles/Picker.css';
 import '../../styles/ColorPicker.css';
 
-export const ColorPicker = ({current, readOnly, onSelect}) => {
-  return (
-    <div className={'Picker ColorPicker' + (!!readOnly ? ' readOnly' : '')}>
-      <div className="Picker-content">
-        {colors.map(color => {
-          return (<a className={'Picker-item' + (current === color ? ' active' : '')}
-                     key={color}
-                     onClick={() => !readOnly && onSelect(color)}
-                     style={{backgroundColor: color}}/>);
-        })}
-    </div>
-  </div>
-  );
-};
+//todo - this has a lot of logic shared with Symbol Picker, but some differences in data structure etc. Should probably merge them though.
 
-ColorPicker.propTypes = {
-  readOnly: PropTypes.bool,
-  current: PropTypes.string,
-  onSelect: PropTypes.func,
-};
+export default class ColorPicker extends Component {
+  static propTypes = {
+    readOnly: PropTypes.bool,
+    current: PropTypes.string,
+    onSelect: PropTypes.func,
+  };
 
-export default ColorPicker;
+  constructor(props) {
+    super(props);
+    this.state = {
+      showContent: false,
+      hoverText: this.nameColor(props.current),
+    };
+  }
+
+  nameColor(color) {
+    return colorMap[color] || '';
+  }
+
+  onClickCurrent = () => {
+    const handleDocumentClick = (evt) => {
+      if (this.pickerToggler.contains(evt.target)) return;
+
+      this.setState({ showContent: false });
+      document.removeEventListener('click', handleDocumentClick);
+    };
+    if (this.state.showContent) return; //dont register more than once
+
+    document.addEventListener('click', handleDocumentClick);
+    this.setState({ showContent: true });
+  };
+
+  onMouseEnter = (hoverText) => {
+    this.setState({ hoverText: this.nameColor(hoverText) });
+  };
+
+  onMouseOut = () => {
+    this.setState({ hoverText: this.nameColor(this.props.current) });
+  };
+
+  render() {
+    const { current, readOnly, onSelect } = this.props;
+
+    return (
+      <div className={'Picker ColorPicker' + (!!readOnly ? ' readOnly' : '')}>
+        <div ref={ref => this.pickerToggler = ref}
+             className="Picker-current"
+             onClick={this.onClickCurrent}>
+          <PickerItem isCurrent={false}
+                      styles={{backgroundColor: current}}/>
+        </div>
+        {this.state.showContent && (
+          <div className="Picker-content"
+               onMouseOut={this.onMouseOut}>
+            <div className="Picker-currentHovered">{this.state.hoverText}</div>
+            <div className="Picker-options">
+              {colors.map(color => {
+                return (<PickerItem key={color}
+                                    isCurrent={current === color}
+                                    onMouseEnter={() => this.onMouseEnter(color)}
+                                    onClick={() => !readOnly && onSelect(color)}
+                                    styles={{backgroundColor: color}}/>);
+              })}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
--- src/components/ui/SymbolPicker.js
+++ src/components/ui/SymbolPicker.js
@@ -1,37 +1,86 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import symbols from '../../inventory/sbol';
-import SvgSbol from '../svgsbol';
+import PickerItem from './PickerItem';
 
 import '../../styles/Picker.css';
 import '../../styles/SymbolPicker.css';
 
-export const SymbolPicker = ({current, readOnly, onSelect}) => {
-  return (
-    <div className={'Picker SymbolPicker' + (!!readOnly ? ' readOnly' : '')}>
-      <div className="Picker-content">
-        {symbols.map(symbolObj => {
-          const symbol = symbolObj.id;
-          return (<a className={'Picker-item' + (current === symbol ? ' active' : '')}
-                     alt={symbolObj.name}
-                     title={symbolObj.name}
-                     key={symbol}
-                     onClick={() => !readOnly && onSelect(symbol)}>
-                     <SvgSbol stroke={0.5} width="100%" height="100%" color="white" symbolName={symbol} key={symbol}/>
-                   </a>
-                 );
-        })}
-        <a className={'Picker-item' + (!current ? ' active' : '')}
-           style={{backgroundImage: `url(/images/sbolSymbols/thin/no_symbol.svg)`}}
-           onClick={() => !readOnly && onSelect(null)}/>
+export default class SymbolPicker extends Component {
+  static propTypes = {
+    readOnly: PropTypes.bool,
+    current: PropTypes.string,
+    onSelect: PropTypes.func,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showContent: false,
+      hoverText: props.current || '',
+    };
+  }
+
+  onClickCurrent = () => {
+    const handleDocumentClick = (evt) => {
+      if (this.pickerToggler.contains(evt.target)) return;
+
+      this.setState({showContent: false});
+      document.removeEventListener('click', handleDocumentClick);
+    };
+    if (this.state.showContent) return; //dont register more than once
+
+    document.addEventListener('click', handleDocumentClick);
+    this.setState({showContent: true});
+  };
+
+  onMouseEnter = (hoverText) => {
+    this.setState({ hoverText });
+  };
+
+  onMouseOut = () => {
+    const def = 'No Symbol';
+    this.setState({ hoverText: this.props.current || def });
+  };
+
+  render() {
+    const { current, readOnly, onSelect } = this.props;
+    //todo - handle no symbol in Picker-current
+    const noSymbol = 'noSymbol';
+    const hoverTextDefault = 'No Symbol';
+
+    return (
+      <div className={'Picker SymbolPicker' + (!!readOnly ? ' readOnly' : '')}>
+        <div className="Picker-current"
+             ref={ref => this.pickerToggler = ref}
+             onClick={this.onClickCurrent}>
+          <PickerItem isCurrent={false}
+                      svg={current || noSymbol}/>
+        </div>
+        {this.state.showContent && (
+          <div className="Picker-content"
+               onMouseOut={this.onMouseOut}>
+            <div className="Picker-currentHovered">{this.state.hoverText || hoverTextDefault}</div>
+            <div className="Picker-options">
+              {symbols.map(symbolObj => {
+                const { id, name } = symbolObj;
+
+                return (<PickerItem key={id}
+                                    isCurrent={current === id}
+                                    name={name}
+                                    svg={id}
+                                    onMouseEnter={() => this.onMouseEnter(id)}
+                                    onClick={() => !readOnly && onSelect(id)}
+                />);
+              })}
+              <PickerItem isCurrent={!current}
+                          name={'No Symbol'}
+                          svg={noSymbol}
+                          onClick={() => !readOnly && onSelect(null)}
+              />
+            </div>
+          </div>
+        )}
       </div>
-    </div>
-  );
-};
-
-SymbolPicker.propTypes = {
-  readOnly: PropTypes.bool,
-  current: PropTypes.string,
-  onSelect: PropTypes.func,
-};
-
-export default SymbolPicker;
+    );
+  }
+}
