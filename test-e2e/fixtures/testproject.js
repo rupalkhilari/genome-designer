b646c31e81f7355443a873fd6b3bd4c26480dd11
Duncan Meech <duncanmeech@gmail.com>
2016-04-29 10:22:30 -0700

    add testproject fixture, update block delete test

---
+++ test-e2e/fixtures/testproject.js
@@ -0,0 +1,38 @@
+var dragFromTo = require('./dragfromto');
+var newproject = function(browser) {
+  browser
+    // make sure inventory is present
+    .waitForElementPresent('.SidePanel.Inventory', 5000, 'Expected Inventory Groups')
+    // open the file menu and add new constructs
+    .click('.menu-dropdown:nth-of-type(1)')
+    .waitForElementPresent('.menu-header-open', 5000, 'expected an open menu')
+    // click new construct menu item
+    .click('.menu-dropdown:nth-of-type(1) .menu-item:nth-of-type(2)')
+    .waitForElementNotPresent('.menu-header-open', 5000, 'expected a closed menu')
+    .waitForElementPresent('.construct-viewer', 5000, 'expect a construct for the new project')
+    // click the second inventory group 'EGF Parts' to open it
+    .click('.InventoryGroup:nth-of-type(2) .InventoryGroup-heading')
+    // expect at least one inventory item and one block to drop on
+    .waitForElementPresent('.InventoryItem', 5000, 'expected an inventory item');
+
+  // drag 3 egf parts into construct
+  dragFromTo(browser, '.InventoryItem:nth-of-type(1)', 10, 10, '.construct-viewer:nth-of-type(1) .sceneGraph', 700, 30);
+  dragFromTo(browser, '.InventoryItem:nth-of-type(2)', 10, 10, '.construct-viewer:nth-of-type(1) .sceneGraph', 700, 30);
+  dragFromTo(browser, '.InventoryItem:nth-of-type(3)', 10, 10, '.construct-viewer:nth-of-type(1) .sceneGraph', 700, 30);
+
+  browser
+    .click('.InventoryGroup:nth-of-type(4) .InventoryGroup-heading')
+    .pause(500)
+    .waitForElementPresent('.InventoryItem', 5000, 'expected an inventory item');
+
+  // drag 3 sbol symbols into construct
+  dragFromTo(browser, '.InventoryItem:nth-of-type(1)', 10, 10, '.construct-viewer:nth-of-type(1) .sceneGraph', 700, 30);
+  dragFromTo(browser, '.InventoryItem:nth-of-type(2)', 10, 10, '.construct-viewer:nth-of-type(1) .sceneGraph', 700, 30);
+  dragFromTo(browser, '.InventoryItem:nth-of-type(3)', 10, 10, '.construct-viewer:nth-of-type(1) .sceneGraph', 700, 30);
+
+  browser
+    .pause(250)
+    .assert.countelements('.sbol-glyph', 6);
+};
+
+module.exports = newproject;
--- test-e2e/tests/block-delete-context-menu.js
+++ test-e2e/tests/block-delete-context-menu.js
@@ -4,6 +4,7 @@ var signin = require('../fixtures/signin');
 var dragFromTo = require('../fixtures/dragfromto');
 var newProject = require('../fixtures/newproject');
 var newConstruct = require('../fixtures/newconstruct');
+var testProject = require('../fixtures/testproject');
 var openNthBlockContextMenu = require('../fixtures/open-nth-block-contextmenu');
 var clickNthContextMenuItem = require('../fixtures/click-popmenu-nth-item');
 
@@ -18,28 +19,25 @@ module.exports = {
 
     // now we can go to the project page
     browser
-      .url('http://localhost:3001/project/test')
       // wait for inventory and inspector to be present
       .waitForElementPresent('.SidePanel.Inventory', 5000, 'Expected Inventory Groups')
       .waitForElementPresent('.SidePanel.Inspector', 5000, 'Expected Inspector')
       // open inventory
       .click('.Inventory-trigger')
-      .waitForElementPresent('.SidePanel.Inventory.visible', 5000, 'Expected inventory to be visible')
-      // click the second inventory group 'EGF Parts' to open it
-      .click('.InventoryGroup:nth-of-type(2) .InventoryGroup-heading')
-      // verify starting blocks
-      .assert.countelements('.sbol-glyph', 8)
-      .pause(1000)
+      .waitForElementPresent('.SidePanel.Inventory.visible', 5000, 'Expected inventory to be visible');
 
+    testProject(browser);
 
     // delete block from second construct viewer
     var blockBounds = openNthBlockContextMenu(browser, '.construct-viewer:nth-of-type(1) .sceneGraph', 0);
     clickNthContextMenuItem(browser, 3);
 
-    // expect the block to be deleted
-    browser.assert.countelements('.sbol-glyph', 7)
+    // NOTE: The last item add will be selected. Clicking the first item will group select all blocks
 
-    // all done
-    browser.end();
+    // expect all blocks to be deleted
+    browser
+      .pause(250)
+      .assert.countelements('.sbol-glyph', 0)
+      .end();
   }
 };
