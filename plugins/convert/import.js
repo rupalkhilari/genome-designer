26091fa720413b53053d03dbff04da1a86592444
Duncan Meech <duncanmeech@gmail.com>
2016-05-02 13:08:57 -0700

    change cvs to csv everywhere, make import paths use formidable for reading temp files versus body of requrest

--- plugins/convert/import.js
+++ plugins/convert/import.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { getPlugin } from '../loadPlugin';
+import {getPlugin} from '../loadPlugin';
 import * as persistence from '../../server/data/persistence';
 import * as rollup from '../../server/data/rollup';
 import _ from 'lodash';
+import fs from 'fs';
+import formidable from 'formidable';
 
 const router = express.Router(); //eslint-disable-line new-cap
 const jsonParser = bodyParser.json({
@@ -14,26 +16,26 @@ const namespace = 'convert';
 
 function callImportFunction(funcName, pluginId, data) {
   return getPlugin(namespace, pluginId)
-    .then(mod => {
-      return new Promise((resolve, reject) => {
-        if (mod && mod[funcName]) {
-          try {
-            const func = mod[funcName];
-            func(data)
-              .then(res => {
-                resolve(res);
-              })
-              .catch(err => {
-                reject(err);
-              });
-          } catch (err) {
+  .then(mod => {
+    return new Promise((resolve, reject) => {
+      if (mod && mod[funcName]) {
+        try {
+          const func = mod[funcName];
+          func(data)
+          .then(res => {
+            resolve(res);
+          })
+          .catch(err => {
             reject(err);
-          }
-        } else {
-          reject('No import option named ' + pluginId + ' for projects');
+          });
+        } catch (err) {
+          reject(err);
         }
-      });
+      } else {
+        reject('No import option named ' + pluginId + ' for projects');
+      }
     });
+  });
 }
 
 function importProject(pluginId, data) {
@@ -42,53 +44,53 @@ function importProject(pluginId, data) {
 
 //can pass :projectId = 'convert' to just convert genbank file directly, and not write to memory
 router.post('/:pluginId/:projectId?', jsonParser, (req, resp, next) => {
-  const { pluginId, projectId } = req.params;
+  const {pluginId, projectId} = req.params;
 
-  //assuming contents to be string
-  let buffer = '';
+  // save incoming file then read back the string data.
+  // If these files turn out to be large we could modify the import functions to take
+  // file names instead but for now, in memory is fine.
+  const form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
 
-  //get data in parts
-  req.on('data', data => {
-    buffer += data;
-  });
+    fs.readFile(files.data.path, 'utf8', (err, data) => {
 
-  //received all the data
-  req.on('end', () => {
-    if (projectId === 'convert') {
-      return callImportFunction('convert', pluginId, buffer)
+      // needed for genbank without a project.
+      if (projectId === 'convert') {
+        return callImportFunction('convert', pluginId, data)
         .then(converted => resp.status(200).json(converted))
         .catch(err => next(err));
-    }
+      }
 
-    return importProject(pluginId, buffer)
+      return importProject(pluginId, data)
       .then((roll) => {
         if (!projectId) {
           rollup.writeProjectRollup(roll.project.id, roll, req.user.uuid)
-            .then(() => persistence.projectSave(roll.project.id))
-            .then(commit => resp.status(200).json({ ProjectId: roll.project.id }))
-            .catch(err => {
-              resp.status(400).send(err);
-            });
+          .then(() => persistence.projectSave(roll.project.id))
+          .then(commit => resp.status(200).json({ProjectId: roll.project.id}))
+          .catch(err => {
+            resp.status(400).send(err);
+          });
         } else {
           rollup.getProjectRollup(projectId)
-            .then((existingRoll) => {
-              existingRoll.project.components = existingRoll.project.components.concat(roll.project.components);
-              existingRoll.blocks = existingRoll.blocks.concat(roll.blocks);
-              rollup.writeProjectRollup(existingRoll.project.id, existingRoll, req.user.uuid)
-                .then(() => persistence.projectSave(existingRoll.project.id))
-                .then(commit => resp.status(200).json({ ProjectId: existingRoll.project.id }))
-                .catch(err => {
-                  resp.status(400).send(err);
-                });
-            })
+          .then((existingRoll) => {
+            existingRoll.project.components = existingRoll.project.components.concat(roll.project.components);
+            existingRoll.blocks = existingRoll.blocks.concat(roll.blocks);
+            rollup.writeProjectRollup(existingRoll.project.id, existingRoll, req.user.uuid)
+            .then(() => persistence.projectSave(existingRoll.project.id))
+            .then(commit => resp.status(200).json({ProjectId: existingRoll.project.id}))
             .catch(err => {
               resp.status(400).send(err);
             });
+          })
+          .catch(err => {
+            resp.status(400).send(err);
+          });
         }
       })
       .catch(err => {
         resp.status(500).send(err);
       });
+    });
   });
 });
 
