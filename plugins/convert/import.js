5f79bf04f9435eb889dade09559ac3bb7034be65
Duncan Meech <duncanmeech@gmail.com>
2016-05-03 11:41:12 -0700

    fix up import for NCBI genbank files

--- plugins/convert/import.js
+++ plugins/convert/import.js
@@ -48,50 +48,57 @@ router.post('/:pluginId/:projectId?', jsonParser, (req, resp, next) => {
 
   // needed for genbank without a project.
   if (projectId === 'convert') {
-    return callImportFunction('convert', pluginId, data)
-    .then(converted => resp.status(200).json(converted))
-    .catch(err => next(err));
-  }
+    let buffer = '';
+    req.on('data', data => {
+      buffer += data;
+    });
+    req.on('end', () => {
+      return callImportFunction('convert', pluginId, buffer)
+        .then(converted => resp.status(200).json(converted))
+        .catch(err => next(err));
+    });
+  } else {
 
-  // save incoming file then read back the string data.
-  // If these files turn out to be large we could modify the import functions to take
-  // file names instead but for now, in memory is fine.
-  const form = new formidable.IncomingForm();
-  form.parse(req, (err, fields, files) => {
+      // save incoming file then read back the string data.
+      // If these files turn out to be large we could modify the import functions to take
+      // file names instead but for now, in memory is fine.
+      const form = new formidable.IncomingForm();
+      form.parse(req, (err, fields, files) => {
 
-    fs.readFile(files.data.path, 'utf8', (err, data) => {
+        fs.readFile(files.data.path, 'utf8', (err, data) => {
 
-      return importProject(pluginId, data)
-      .then((roll) => {
-        if (!projectId) {
-          rollup.writeProjectRollup(roll.project.id, roll, req.user.uuid)
-          .then(() => persistence.projectSave(roll.project.id))
-          .then(commit => resp.status(200).json({ProjectId: roll.project.id}))
-          .catch(err => {
-            resp.status(400).send(err);
-          });
-        } else {
-          rollup.getProjectRollup(projectId)
-          .then((existingRoll) => {
-            existingRoll.project.components = existingRoll.project.components.concat(roll.project.components);
-            existingRoll.blocks = existingRoll.blocks.concat(roll.blocks);
-            rollup.writeProjectRollup(existingRoll.project.id, existingRoll, req.user.uuid)
-            .then(() => persistence.projectSave(existingRoll.project.id))
-            .then(commit => resp.status(200).json({ProjectId: existingRoll.project.id}))
-            .catch(err => {
-              resp.status(400).send(err);
-            });
+          return importProject(pluginId, data)
+          .then((roll) => {
+            if (!projectId) {
+              rollup.writeProjectRollup(roll.project.id, roll, req.user.uuid)
+              .then(() => persistence.projectSave(roll.project.id))
+              .then(commit => resp.status(200).json({ProjectId: roll.project.id}))
+              .catch(err => {
+                resp.status(400).send(err);
+              });
+            } else {
+              rollup.getProjectRollup(projectId)
+              .then((existingRoll) => {
+                existingRoll.project.components = existingRoll.project.components.concat(roll.project.components);
+                existingRoll.blocks = existingRoll.blocks.concat(roll.blocks);
+                rollup.writeProjectRollup(existingRoll.project.id, existingRoll, req.user.uuid)
+                .then(() => persistence.projectSave(existingRoll.project.id))
+                .then(commit => resp.status(200).json({ProjectId: existingRoll.project.id}))
+                .catch(err => {
+                  resp.status(400).send(err);
+                });
+              })
+              .catch(err => {
+                resp.status(400).send(err);
+              });
+            }
           })
           .catch(err => {
-            resp.status(400).send(err);
+            resp.status(500).send(err);
           });
-        }
-      })
-      .catch(err => {
-        resp.status(500).send(err);
+        });
       });
-    });
-  });
+    }
 });
 
 //export these functions for testing purpose
