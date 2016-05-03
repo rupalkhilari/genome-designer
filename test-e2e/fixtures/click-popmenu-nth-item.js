be5c0c4f5f1f85a9ec298fd96c5b2893c415fb68
Duncan Meech <duncanmeech@gmail.com>
2016-05-03 10:26:14 -0700

    merge with master and fix e2e tests ( except genbank import/export for which there is a pending PR

--- test-e2e/fixtures/click-popmenu-nth-item.js
+++ test-e2e/fixtures/click-popmenu-nth-item.js
@@ -1,7 +1,6 @@
 var clickcontextmenu = function(browser, index) {
   browser
     .waitForElementPresent('.menu-popup-blocker-visible .menu-popup-container', 5000, 'expected an open menu')
-    // click new construct menu item
     .click('.menu-popup-blocker-visible .menu-popup-container .menu-item:nth-of-type(' + index + ')')
     .waitForElementNotPresent('.menu-popup-blocker-visible .menu-popup-container', 5000, 'expected a closed menu')
 };
