90d6e88806420d798bf32597b50ca1e2378c2ead
Duncan Meech <duncanmeech@gmail.com>
2016-05-04 15:24:10 -0700

    add fixture for opening a context menu in nightwatch, add test for deleting a construct from the context menu

---
+++ test-e2e/tests/delete-construct.js
@@ -0,0 +1,29 @@
+var homepageRegister = require('../fixtures/homepage-register');
+var newConstruct = require('../fixtures/newconstruct');
+var clickAt = require('../fixtures/clickAt');
+var rightClickAt = require('../fixtures/rightClickAt');
+var clickContextMenu = require('../fixtures/click-popmenu-nth-item.js');
+
+module.exports = {
+  'Test deleting a construct using construct menu in header' : function (browser) {
+    var credentials = homepageRegister(browser);
+    browser
+      // wait for inventory and inspector to be present to ensure app is ready
+      .waitForElementPresent('.SidePanel.Inventory', 5000, 'Expected Inventory Groups')
+      .waitForElementPresent('.SidePanel.Inspector', 5000, 'Expected Inspector');
+
+    newConstruct(browser);
+
+    browser
+      .waitForElementPresent('.construct-viewer', 5000, 'expected one construct viewer')
+      .waitForElementPresent('[data-nodetype="construct-title"]', 5000, 'expected a title for the construct')
+
+    rightClickAt(browser, '[data-nodetype="construct-title"]', 15, 15);
+
+    clickContextMenu(browser, 1);
+
+    browser
+      .waitForElementNotPresent('.construct-viewer', 5000, 'expected construct viewer to go away')
+      .end();
+  }
+};
