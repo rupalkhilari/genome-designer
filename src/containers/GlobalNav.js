5185f64e069b3f8254d51a727217f4492ab6f77b
Maxwell Bates <max.bates@autodesk.com>
2016-05-05 14:16:43 -0700

    don't name New Construct when make project from global nav

--- src/containers/GlobalNav.js
+++ src/containers/GlobalNav.js
@@ -160,7 +160,6 @@ class GlobalNav extends Component {
     const project = this.props.projectCreate();
     // add a construct to the new project
     const block = this.props.blockCreate({projectId: project.id});
-    this.props.blockRename(block.id, 'New Construct');
     this.props.projectAddConstruct(project.id, block.id);
     this.props.focusConstruct(block.id);
     this.props.projectOpen(project.id);
