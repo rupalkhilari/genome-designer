af9fccb1d8dc010ad930dc67fa9ed1740868c0bc
Duncan Meech <duncanmeech@gmail.com>
2016-05-11 12:37:15 -0700

    fix keyboard cat modifications

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,7 +24,7 @@ class App extends Component {
    */
   componentDidMount() {
     document.addEventListener('keydown', this.rejectBackspace);
-    document.addEventLiqstener('keypress', this.rejectBackspace);
+    document.addEventListener('keypress', this.rejectBackspace);
   }
 
   rejectBackspace(evt) {
