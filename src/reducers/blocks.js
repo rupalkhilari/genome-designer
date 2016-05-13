2dacce9454ceab1cebab482b392479634a80d80f
Maxwell Bates <max.bates@autodesk.com>
2016-05-13 13:46:09 -0700

    simplify blocks reducer so all actions can return block or blocks

--- src/reducers/blocks.js
+++ src/reducers/blocks.js
@@ -16,23 +16,24 @@ export default function blocks(state = initialState, action) {
   case ActionTypes.BLOCK_RENAME :
   case ActionTypes.BLOCK_SET_COLOR :
   case ActionTypes.BLOCK_CLONE :
-  case ActionTypes.BLOCK_SET_SBOL :
-  case ActionTypes.BLOCK_ADD_SBOL :
+  case ActionTypes.BLOCK_LOAD :
+  case ActionTypes.BLOCK_STASH :
+  case ActionTypes.BLOCK_SET_ROLE :
+  case ActionTypes.BLOCK_ADD_ROLE :
   case ActionTypes.BLOCK_ANNOTATE :
   case ActionTypes.BLOCK_REMOVE_ANNOTATION :
   case ActionTypes.BLOCK_SET_SEQUENCE :
   case ActionTypes.BLOCK_COMPONENT_ADD :
   case ActionTypes.BLOCK_COMPONENT_MOVE :
   case ActionTypes.BLOCK_COMPONENT_REMOVE :
-    const { block } = action;
-    return Object.assign({}, state, { [block.id]: block });
+    //can pass either block {} or blocks [], precedence for blocks array
+    const { block, blocks } = action;
 
-  case ActionTypes.BLOCK_LOAD :
-  case ActionTypes.BLOCK_STASH :
-    //expects an array of Block models
-    const { blocks } = action;
-    const toMerge = blocks.reduce((acc, block) => Object.assign(acc, { [block.id]: block }), {});
-    return Object.assign({}, state, toMerge);
+    if (Array.isArray(blocks)) {
+      const toMerge = blocks.reduce((acc, block) => Object.assign(acc, { [block.id]: block }), {});
+      return Object.assign({}, state, toMerge);
+    }
+    return Object.assign({}, state, { [block.id]: block });
 
   case ActionTypes.BLOCK_DELETE :
     const { blockId } = action;
