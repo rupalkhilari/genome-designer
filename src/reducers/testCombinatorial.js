21a3d92d5c49157dd6d3d85638db81ccdb48fe95
Maxwell Bates <max.bates@autodesk.com>
2016-05-19 10:53:16 -0700

    include an option in list blocks

--- src/reducers/testCombinatorial.js
+++ src/reducers/testCombinatorial.js
@@ -1,17 +1,26 @@
 import Block from '../models/Block';
 import Project from '../models/Project';
 
-const lists = ['1', '2', '3', '4,', '5', '6', '7'].map(pos => new Block({
-  metadata: {
-    name: `Position ${pos}`,
-  },
-  rules: {
-    list: true,
-    filter: {
-      'metadata.egfPosition': pos,
+import parts from '../inventory/andrea/parts';
+
+const lists = ['1', '2', '3', '4', '5', '6', '7', '8'].map(pos => {
+  const firstpart = parts.find(part => part.metadata.egfPosition === pos);
+
+  return new Block({
+    metadata: {
+      name: `Position ${pos}`,
     },
-  },
-}));
+    options: [
+      firstpart.id,
+    ],
+    rules: {
+      list: true,
+      filter: {
+        'metadata.egfPosition': pos,
+      },
+    },
+  });
+});
 
 const construct = new Block({
   metadata: {
