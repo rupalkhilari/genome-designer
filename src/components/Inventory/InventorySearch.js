fc2061903d409ea392445db9d1192e0039ea6619
Maxwell Bates <max.bates@autodesk.com>
2016-05-23 13:53:32 -0700

    autofocus search, at least when inventory already open

--- src/components/Inventory/InventorySearch.js
+++ src/components/Inventory/InventorySearch.js
@@ -19,9 +19,11 @@ export default class InventorySearch extends Component {
     return (
       <div className={'InventorySearch' + (this.props.isSearching ? ' searching' : '')}>
         <input className="InventorySearch-input"
+               autoFocus
                type="text"
                disabled={!!this.props.disabled ? true : null}
                value={this.props.searchTerm}
+               maxLength={50}
                placeholder={this.props.placeholder || 'Keyword, biological function'}
                onChange={this.handleSearchChange} />
         <div className="InventorySearch-progress"></div>
