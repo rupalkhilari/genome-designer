fa10de3f3f942bc261fcf7f9a443773440b6b409
Maxwell Bates <max.bates@autodesk.com>
2016-06-01 13:05:27 -0700

    restyle search sources to use popup menu

--- src/components/Inventory/InventorySources.js
+++ src/components/Inventory/InventorySources.js
@@ -3,37 +3,36 @@ import React, { PropTypes } from 'react';
 import '../../styles/InventorySources.css';
 
 export default function InventorySources({ toggling, sourceList, registry, onSourceToggle, onToggleVisible }) {
-  const content = !toggling
-    ?
-    <span>{`${sourceList.map(source => registry[source].name).join(', ')}`}</span>
-    :
-    <div>
-      {Object.keys(registry)
-        .filter(key => typeof registry[key].search === 'function')
-        .map(key => {
-          const source = registry[key];
-          return (
-            <div key={key}
-                 onClick={(evt) => {
-                   evt.stopPropagation();
-                   onSourceToggle(key);
-                 }}
-                 className="InventorySources-source">
-              <input type="checkbox"
-                     className="InventorySources-toggler"
-                     readOnly
-                     checked={sourceList.includes(key)}/>{source.name}</div>
-          );
-        })}
-    </div>;
+  const menu = (<div className="InventorySources-menu menu-popup-container">
+    <div className="InventorySources-menu-heading menu-item disabled">Search Sources:</div>
+    {Object.keys(registry)
+      .filter(key => typeof registry[key].search === 'function')
+      .map(key => {
+        const source = registry[key];
+        return (
+          <div key={key}
+               onClick={(evt) => {
+                 evt.stopPropagation();
+                 onSourceToggle(key);
+               }}
+               className={'InventorySources-menu-source menu-item'}>
+            <div className={(sourceList.includes(key) ? 'menu-item-checked' : 'menu-item-unchecked')}></div>
+            {source.name}
+          </div>
+        );
+      })}
+  </div>);
 
   return (
-    <div className={'InventorySources' + (toggling ? ' expanded' : '')} onClick={() => onToggleVisible()}>
-      <div className="InventorySources-cog"></div>
-      <div className="InventorySources-sources">
-        <span>Sources: </span>
-        {content}
+    <div className={'InventorySources' + (toggling ? ' expanded' : '')}
+         onClick={() => onToggleVisible()}>
+      <div className="InventorySources-back">
+        <div className="InventorySources-back-cog"></div>
+        <div className="InventorySources-back-sources">
+          <span>{`Sources: ${sourceList.map(source => registry[source].name).join(', ')}`}</span>
+        </div>
       </div>
+      {toggling && menu}
     </div>
   );
 }
