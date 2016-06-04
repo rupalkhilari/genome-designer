febcecc4d5709dd275fbc5ba86f0c65cc8e13cc4
Maxwell Bates <max.bates@autodesk.com>
2016-06-03 19:11:06 -0700

    basic updaing of role map (hackish) when roles change... cant think of a good way to handle this...

--- src/components/Inventory/InventoryRoleMap.js
+++ src/components/Inventory/InventoryRoleMap.js
@@ -5,6 +5,10 @@ import { block as blockDragType } from '../../constants/DragTypes';
 import { infoQuery } from '../../middleware/data';
 import { symbolMap } from '../../inventory/roles';
 
+//bit of a hack, since we are not storing this information in the store (since its really derived data) but need to update it
+import { lastAction, subscribe } from '../../store/index';
+import * as ActionTypes from '../../constants/ActionTypes';
+
 import InventoryListGroup from './InventoryListGroup';
 import InventoryList from './InventoryList';
 import Spinner from '../ui/Spinner';
@@ -27,9 +31,40 @@ export class InventoryRoleMap extends Component {
       typeMap,
       loadingMap: false,
     }));
+
+    //update the state map when roles change (since otherwise we only upload when component loads)
+    //hack - ideally this would be derived data. However, to derive, need all the blocks in the store. So, we'll patch it.
+    //apolgies for denseness, but intend to get rid of this before too long
+    this.storeSubscriber = subscribe(() => {
+      const action = lastAction();
+      //todo - should handle creating new blocks and stuff
+      //usually, not looking at this section when creating new blocks? ignoring for now...
+      if (action.type === ActionTypes.BLOCK_SET_ROLE) {
+        const { oldRole, block } = action;
+        const oldRoleEff = oldRole || 'none';
+        const newRole = block.rules.role || 'none';
+
+        const newTypeMap = Object.assign({}, this.state.typeMap, {
+          [oldRoleEff]: this.state.typeMap[oldRoleEff] - 1,
+          [newRole]: (this.state.typeMap[newRole] || 0) + 1,
+        });
+        const newLoadedTypes = Object.assign({}, this.state.loadedTypes, {
+          [oldRoleEff]: (this.state.loadedTypes[oldRoleEff] || []).filter(roleBlock => block.id !== roleBlock.id),
+          [newRole]: [...(this.state.loadedTypes[newRole] || []), block],
+        });
+        this.setState({
+          typeMap: newTypeMap,
+          loadedTypes: newLoadedTypes,
+        });
+      }
+    });
   }
 
-  //null is for loading
+  componentWillUnmount() {
+    this.storeSubscriber();
+  }
+
+  //false is for loading
   setRoleType(type, blocks = false) {
     this.setState({
       loadedTypes: Object.assign(this.state.loadedTypes, { [type]: blocks }),
