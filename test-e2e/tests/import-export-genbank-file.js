a3f18b249e702afe632bbf877c1adf9f03717810
Duncan Meech <duncanmeech@gmail.com>
2016-06-10 09:06:54 -0700

    fix import/export genbank file

--- test-e2e/tests/import-export-genbank-file.js
+++ test-e2e/tests/import-export-genbank-file.js
@@ -24,7 +24,7 @@ module.exports = {
     newProject(browser);
 
     // click the file menu -> Upload Genbank File
-    clickMainMenu(browser, 1, 8);
+    clickMainMenu(browser, 1, 6);
 
     browser
       .waitForElementPresent('.genbank-import-form', 5000, 'Expect the import dialog to appear')
