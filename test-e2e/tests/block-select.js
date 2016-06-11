0230ce3a00f0810e2389ee376918731de9f7720d
Duncan Meech <duncanmeech@gmail.com>
2016-06-11 06:13:56 -0700

    rewrite block select test to remove flakiness

--- test-e2e/tests/block-select.js
+++ test-e2e/tests/block-select.js
@@ -3,7 +3,7 @@ var signout = require('../fixtures/signout');
 var signin = require('../fixtures/signin');
 var dragFromTo = require('../fixtures/dragfromto');
 var dragRegion = require('../fixtures/dragregion');
-var newProject = require('../fixtures/newproject');
+var testProject = require('../fixtures/testproject');
 var newConstruct = require('../fixtures/newconstruct');
 var clickNthBlock = require('../fixtures/click-nth-block-bounds');
 var clickAt = require('../fixtures/clickAt');
@@ -12,71 +12,31 @@ var openInventory = require('../fixtures/open-inventory');
 module.exports = {
   'Test that you can shift/meta/fence select blocks' : function (browser) {
 
-    // maximize for graphical tests
     browser.windowSize('current', 1200, 900);
+    homepageRegister(browser);
+    testProject(browser);
 
-    // register via fixture
-    var credentials = homepageRegister(browser);
-
-    // now we can go to the project page
-    browser
-      // wait for inventory and inspector to be present
-      .waitForElementPresent('.SidePanel.Inventory', 5000, 'Expected Inventory Groups')
-      .waitForElementPresent('.SidePanel.Inspector', 5000, 'Expected Inspector');
-
-    // start with a fresh project, single construct
-    newProject(browser);
-
-      // open inventory
-    openInventory(browser);
-    browser
-      // sketch blocks
-      .click('.InventoryGroup:nth-of-type(3) .InventoryGroup-heading');
-
-    // expect one construct
-    browser.assert.countelements('.construct-viewer', 1);
-
-    // add a single block
-    dragFromTo(browser, '.InventoryItemRole:nth-of-type(1)', 10, 10, '.construct-viewer:nth-of-type(1)', 100, 70);
+    // click first block
+    clickNthBlock(browser, '.sceneGraph', 0);
 
     browser
-      // expect one block
-      .assert.countelements('[data-nodetype="block"]', 1)
-      .pause(500);
-
-    // drag some other blocks into the construct
-    // drag an item from the inventory
-
-    for(var i = 1; i < 10; i += 1) {
-      dragFromTo(
-          browser,
-          '.InventoryItemRole:nth-of-type(' + i + ')', 10, 10,
-          '.construct-viewer:nth-of-type(1) .sceneGraph [data-nodetype="block"]', 10, 10);
-      browser.pause(50);
-    }
-
-    // should have 10 blocks total
-    browser.assert.countelements('[data-nodetype="block"]', 10);
-    var blockBounds = clickNthBlock(browser, '.sceneGraph', 0);
-
-    // expect 1 selection block
-    browser
+      .pause(100)
       .waitForElementPresent(".scenegraph-userinterface-selection", 5000, 'expected a selection block')
       .assert.countelements(".scenegraph-userinterface-selection", 1);
 
-    // shift click all 10 blocks and expect to see 10 selection blocks
+    // shift click all 6 blocks and expect to see 10 selection blocks
     browser.execute(function() {
       window.__gde2e = {shiftKey: true}
     }, [], function() {});
 
-    for(var i = 0; i < 10; i += 1) {
-      var blockBounds = clickNthBlock(browser, '.sceneGraph', i);
+    for(var i = 0; i < 6; i += 1) {
+      clickNthBlock(browser, '.sceneGraph', i);
     }
 
-    // expect all 10 elements to be selected
+    // expect all 6 elements to be selected
     browser
       .pause(250)
-      .assert.countelements(".scenegraph-userinterface-selection", 10);
+      .assert.countelements(".scenegraph-userinterface-selection", 6);
 
     // turn off shift key and test that meta key toggles a blocks selection state
     browser.execute(function() {
@@ -88,8 +48,8 @@ module.exports = {
 
     clickNthBlock(browser, '.sceneGraph', 5);
     browser.pause(1000);
-    // expect only 9 elements to be selected
-    browser.assert.countelements(".scenegraph-userinterface-selection", 9);
+    // expect only 5 elements to be selected
+    browser.assert.countelements(".scenegraph-userinterface-selection", 5);
 
     // click outside the blocks to deselect them all
     clickAt(browser, '.scenegraph-userinterface', 10, 10);
@@ -108,7 +68,7 @@ module.exports = {
     browser
       .waitForElementPresent('.scenegraph-userinterface-selection', 5000, 'expected selections')
       // ensure we have all elements selected
-      .assert.countelements(".scenegraph-userinterface-selection", 10)
+      .assert.countelements(".scenegraph-userinterface-selection", 6)
       .end();
   }
 };
