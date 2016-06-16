1ce79c1769a1f9008c94f6d3beb8af3f9f5a1bbf
Duncan Meech <duncanmeech@gmail.com>
2016-06-16 10:46:18 -0700

    remove the hack for shift key / meta key clicks and replace with correct selenium call

--- test-e2e/tests/block-select.js
+++ test-e2e/tests/block-select.js
@@ -24,12 +24,9 @@ module.exports = {
       .waitForElementPresent(".scenegraph-userinterface-selection", 5000, 'expected a selection block')
       .assert.countelements(".scenegraph-userinterface-selection", 1);
 
-    // shift click all 6 blocks and expect to see 10 selection blocks
-    browser.execute(function() {
-      window.__gde2e = {shiftKey: true}
-    }, [], function() {});
-
-    browser.pause(2000);
+    browser
+      .pause(3000)
+      .keys([browser.Keys.SHIFT]);
 
     for(var i = 0; i < 6; i += 1) {
       clickNthBlock(browser, '.sceneGraph', i);
@@ -40,34 +37,16 @@ module.exports = {
       .pause(250)
       .assert.countelements(".scenegraph-userinterface-selection", 6);
 
-    // turn off shift key and test that meta key toggles a blocks selection state
-    browser.execute(function() {
-      window.__gde2e = {
-        shiftKey: false,
-        metaKey: true
-      }
-    }, [], function() {
-
-    });
-    browser.pause(2000);
-
-    clickNthBlock(browser, '.sceneGraph', 5);
-    browser.pause(1000);
-    // expect only 5 elements to be selected
+    // meta key select 1 block to toggle it
+    // meta key select 1 block to toggle it
+    browser.keys([browser.Keys.META]);
+    clickNthBlock(browser, '.sceneGraph', 0);
     browser.assert.countelements(".scenegraph-userinterface-selection", 5);
 
-    // click outside the blocks to deselect them all
+      // click outside the blocks to deselect them all
     clickAt(browser, '.scenegraph-userinterface', 10, 10);
     browser.pause(1000);
     browser.assert.countelements(".scenegraph-userinterface-selection", 0);
-
-    // simulate a fence drag over the entire construct to reselect everything
-    browser.execute(function() {
-      window.__gde2e = {
-        shiftKey: false,
-        metaKey: false,
-      }
-    }, [], function() {});
 
     dragRegion(browser, '.scenegraph-userinterface', 10, 10, 870, 160, 100);
     browser
