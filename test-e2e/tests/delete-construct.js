44d965e9632e10b9dd52599b71ed403d38dc6ed5
Duncan Meech <duncanmeech@gmail.com>
2016-06-16 10:58:18 -0700

    make delete construct for reliable

--- test-e2e/tests/delete-construct.js
+++ test-e2e/tests/delete-construct.js
@@ -7,15 +7,11 @@ var clickContextMenu = require('../fixtures/click-popmenu-nth-item.js');
 
 module.exports = {
   'Test deleting a construct using construct menu in header' : function (browser) {
-    var credentials = homepageRegister(browser);
-    browser
-      // wait for inventory and inspector to be present to ensure app is ready
-      .waitForElementPresent('.SidePanel.Inventory', 5000, 'Expected Inventory Groups')
-      .waitForElementPresent('.SidePanel.Inspector', 5000, 'Expected Inspector');
-
+    homepageRegister(browser);
     newProject(browser);
 
     browser
+      .pause(3000)
       .waitForElementPresent('.construct-viewer', 5000, 'expected one construct viewer')
       .waitForElementPresent('[data-nodetype="construct-title"]', 5000, 'expected a title for the construct')
 
