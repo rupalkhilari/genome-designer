fa8fe81c848f60a7af2149c63c16427eda2668fe
Duncan Meech <duncanmeech@gmail.com>
2016-06-16 11:09:58 -0700

    fix timing on drag saved construct test

--- test-e2e/fixtures/newproject.js
+++ test-e2e/fixtures/newproject.js
@@ -1,9 +1,10 @@
 var clickMainMenu = require('./click-main-menu');
 
 var newproject = function(browser) {
+  browser.pause(2000)
   clickMainMenu(browser, 1, 4);
   browser
-    .pause(250)
+    .pause(3000)
     .waitForElementPresent('.construct-viewer', 5000, 'expect a construct for the new project')
 };
 
