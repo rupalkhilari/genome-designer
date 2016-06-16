fc9685996c65de35bd2402f05d8bf86bbd017615
Duncan Meech <duncanmeech@gmail.com>
2016-06-16 11:45:24 -0700

    fix delete project test

--- test-e2e/tests/deleteproject.js
+++ test-e2e/tests/deleteproject.js
@@ -29,9 +29,9 @@ module.exports = {
       .submitForm('form.ok-cancel-form')
       // wait for form to go away
       .waitForElementNotPresent('form.ok-cancel-form', 5000, 'expected confirmation dialog to appear')
-      // expect no blocks since we should be looking at a new project.
-      .waitForElementNotPresent('[data-nodetype="block"]', 5000, 'expected blocks to go away')
-      .assert.countelements('[data-nodetype="block"]', 0)
+      // this will open the templates project with 29 constructs
+      .pause(5000)
+      .assert.countelements('.construct-viewer', 29)
       .end();
   }
 };
