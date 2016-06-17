53ef40373049017aa30f4a84da3c0bad8b6d257b
Duncan Meech <duncanmeech@gmail.com>
2016-06-17 14:51:20 -0700

    remove debugger

--- src/containers/graphics/views/layout.js
+++ src/containers/graphics/views/layout.js
@@ -175,7 +175,11 @@ export default class Layout {
     // the node representing the parent block
     const parentNode = this.nodeFromElement(block.id);
     // get the focused list for this block, if any
-    const focusedOptionId = this.focusedOptions[block.id];
+    let focusedOptionId = this.focusedOptions[block.id];
+    // if nothing focused then default to the first option
+    if (!focusedOptionId && block.options.length) {
+      focusedOptionId = block.options[0].id;
+    }
     // get only the options that are enabled for this block
     const enabled = Object.keys(block.options).filter(opt => block.options[opt]);
     // if block list is empty add a single placeholder block
