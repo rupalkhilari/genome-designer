bad00e5d56e229508302cf44cb728c54d4561fe4
Duncan Meech <duncanmeech@gmail.com>
2016-06-20 13:23:59 -0700

    default to first option selected if none is selected

--- src/containers/graphics/views/layout.js
+++ src/containers/graphics/views/layout.js
@@ -174,12 +174,8 @@ export default class Layout {
     }
     // the node representing the parent block
     const parentNode = this.nodeFromElement(block.id);
-    // get the focused list for this block, if any
-    let focusedOptionId = this.focusedOptions[block.id];
-    // if nothing focused then default to the first option
-    if (!focusedOptionId && block.options.length) {
-      focusedOptionId = block.options[0].id;
-    }
+    // get the focused list for this block, or default to first one
+    let focusedOptionId = this.focusedOptions[block.id] || Object.keys(block.options)[0];
     // get only the options that are enabled for this block
     const enabled = Object.keys(block.options).filter(opt => block.options[opt]);
     // if block list is empty add a single placeholder block
