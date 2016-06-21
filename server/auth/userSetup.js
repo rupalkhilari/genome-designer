d7086fc912f7ff8d33a4fc155010d23ed2d5d746
Maxwell Bates <max.bates@autodesk.com>
2016-06-21 12:30:52 -0700

    create empty project on user setup... not sure how to pass to the client through auth though

--- server/auth/userSetup.js
+++ server/auth/userSetup.js
@@ -1,25 +1,51 @@
+import Block from '../../src/models/Block';
+import Project from '../../src/models/Project';
 import * as rollup from '../data/rollup';
 import * as querying from '../data/querying';
 import makeEgfRollup from '../../data/egf_parts/project';
+import { createRollupFromArray } from '../../server/data/rollup';
+
+const makeEmptyRollup = () => {
+  //use classless when creating so they are not frozen. If they are frozen, may run into issues when writing
+  const construct = Block.classless();
+  const project = Project.classless({
+    components: [construct.id],
+  });
+
+  return createRollupFromArray(project, construct);
+};
 
 //create the EGF project for them
 const createInitialData = (user) => {
-  const egfRollup = makeEgfRollup();
   console.log('[EGF ROLLUP] making rollup ' + egfRollup.project.id + ' for user ' + user.uuid);
+  const egfRollup = makeEgfRollup();
   //console.log(egfRollup);
-  return rollup.writeProjectRollup(egfRollup.project.id, egfRollup, user.uuid);
+
+  const emptyProjectRollup = makeEmptyRollup();
+
+  return Promise.all([
+    rollup.writeProjectRollup(emptyProjectRollup.project.id, emptyProjectRollup, user.uuid),
+    rollup.writeProjectRollup(egfRollup.project.id, egfRollup, user.uuid),
+  ]);
 };
 
 const checkUserSetup = (user) => {
   return querying.listProjectsWithAccess(user.uuid)
-    .then(projects => {
-      if (!projects.length) {
-        return createInitialData(user);
-      }
-    })
     //fixme - sometimes this fails if the user has no projects, so lets just make in that case
     //should update listProjectsWIthAccess
-    .catch((err) => createInitialData(user));
+    .catch((err) => {
+      console.log('error querying for projets');
+      console.log(err);
+      console.log(err.stack);
+      return [];
+    })
+    .then(projects => {
+      if (!projects.length) {
+        // create rollups return ID of empty project
+        return createInitialData(user)
+          .then(rollups => rollups[0].project.id);
+      }
+    });
 };
 
 export default checkUserSetup;
