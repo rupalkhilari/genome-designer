f1def550d023845d702015d9f62e58ed0da4be6f
Maxwell Bates <max.bates@autodesk.com>
2016-06-23 14:55:08 -0700

    dont scaffold block rules

--- src/schemas/Rules.js
+++ src/schemas/Rules.js
@@ -5,23 +5,28 @@ const rulesFields = {
   list: [
     fields.bool(),
     'Block is a list block. It has options rather than components.',
+    { avoidScaffold: true },
   ],
   //see inventory/roles.js for list
   role: [
     fields.string(),
     'Role of the Block (often corresponds to SBOL symbols)',
+    { avoidScaffold: true },
   ],
   hidden: [
     fields.bool(),
     'The block is hidden in the design canvas. It serves as template logic.',
+    { avoidScaffold: true },
   ],
   frozen: [
     fields.bool(),
     'The block is immutable - no changes are allowed',
+    { avoidScaffold: true },
   ],
   fixed: [
     fields.bool(),
     'Block Ids of components are fixed - no movement, insertions, deletions, substitutions',
+    { avoidScaffold: true },
   ],
   /*
    //deprecated filters for now
