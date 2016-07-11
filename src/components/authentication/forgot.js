af3169189449bf56456b33ca35ea030b6ff07097
Duncan Meech <duncanmeech@gmail.com>
2016-07-11 10:24:45 -0700

    fix forgot password API import

--- src/components/authentication/forgot.js
+++ src/components/authentication/forgot.js
@@ -16,7 +16,7 @@ limitations under the License.
 import React, { Component, PropTypes } from 'react';
 import {connect} from 'react-redux';
 import { uiShowAuthenticationForm, uiSetGrunt } from '../../actions/ui';
-import { forgot } from '../../middleware/data';
+import { forgot } from '../../middleware/auth';
 
 /**
  * default visibility and text for error labels
