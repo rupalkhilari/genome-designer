a2a6075acbc8568a66a5fb15bfbb84650330b8b2
Duncan Meech <duncanmeech@gmail.com>
2016-07-13 11:24:11 -0700

    correct test for production v dev env

--- server/server.js
+++ server/server.js
@@ -32,10 +32,7 @@ import exportRouter from '../plugins/convert/export';
 const DEFAULT_PORT = 3000;
 const port = parseInt(process.argv[2], 10) || process.env.PORT || DEFAULT_PORT;
 const hostname = '0.0.0.0';
-
-// not an obvious way to determine if this is production or local. For now I'm testing the port
-// and if it is the HTTP or HTTPS default port we assume production.
-const production = port === 80 || port === 443;
+const production = process.env.NODE_ENV === 'production';
 
 //file paths depending on if building or not
 //note that currently, you basically need to use npm run start in order to serve the client bundle + webpack middleware
