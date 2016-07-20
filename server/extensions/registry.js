23d861d2aa1b452ee65896c28e7eae27f2ce5c8a
Maxwell Bates <max.bates@autodesk.com>
2016-07-20 13:23:00 -0700

    simplify registering of extensions re: build path

--- server/extensions/registry.js
+++ server/extensions/registry.js
@@ -1,18 +1,18 @@
 /*
-Copyright 2016 Autodesk,Inc.
+ Copyright 2016 Autodesk,Inc.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 import path from 'path';
 import fs from 'fs';
 import { pickBy } from 'lodash';
@@ -29,14 +29,9 @@ fs.readdirSync(nodeModulesDir).forEach(packageName => {
       return;
     }
 
-    let depManifest;
-    //building in webpack requires static paths, dynamic requires are really tricky
-    if (process.env.BUILD) {
-      depManifest = require(`gd_extensions/${packageName}/package.json`);
-    } else {
-      const filePath = path.resolve(nodeModulesDir, packageName + '/package.json');
-      depManifest = require(filePath);
-    }
+    //future process.env.BUILD support (if not already handled by line above)
+    const filePath = path.resolve(nodeModulesDir, packageName + '/package.json');
+    const depManifest = require(filePath);
 
     Object.assign(registry, {
       [packageName]: depManifest,
