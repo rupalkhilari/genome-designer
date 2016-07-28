9c8b47df35921d48921d23411c69c3f2431d6c45
Maxwell Bates <max.bates@autodesk.com>
2016-07-28 15:05:04 -0700

    expose middleware for project files at constructor.extensions.files.[read/write]

--- src/extensions/_expose.js
+++ src/extensions/_expose.js
@@ -1,6 +1,7 @@
 import register from './registerExtension';
 import { isRegistered, onRegister, validRegion } from './clientRegistry';
 import { callExtensionApi as api } from '../middleware/extensions';
+import { readProjectFile as read, writeProjectFile as write } from '../middleware/projectFile';
 
 /**
  * API Section for extensions
@@ -9,6 +10,10 @@ import { callExtensionApi as api } from '../middleware/extensions';
 export default {
   register,
   api,
+  files: {
+    read,
+    write,
+  },
   isRegistered,
   onRegister,
   validRegion,
