0ccf6ec1f0ffc4b061ecd0be8438414daee9e7e4
dhylbert <hylbert@gmail.com>
2016-08-02 14:26:14 -0700

    changes for mocha and lint in Jenkins & test fixes

--- test/server/auth/REST.spec.js
+++ test/server/auth/REST.spec.js
@@ -6,38 +6,20 @@ const devServer = require('../../../server/server');
 
 describe('Server', () => {
   describe('auth', () => {
-    let server;
     const dummyUser = {
       email: 'bio.nano.dev@autodesk.com',
       password: 'HelpMe#1',
     };
 
-    before('server setup', (done) => {
-      // this is needed for the middleware functions and could cause conflicts with other tests
-      // recommend creating a global module to provide an app proxy to all tests using the web server from client side
-      // methods
-      server = devServer.listen(3000, 'localhost', function (err) {
-        if (err) {
-          console.log("server failure", err);
-          return done(err);
-        }
-        done();
-      });
-    });
-
-    after(() => {
-      server.close();
-    });
-
     it('/auth/login route should return a 200', (done) => {
-      request(server)
+      request(devServer)
         .post('/auth/login')
         .send(dummyUser)
         .expect(200, done);
     });
 
     it('/auth/login should set a cookie on the client', (done) => {
-      request(server)
+      request(devServer)
         .post('/auth/login')
         .send(dummyUser)
         .expect((res) => {
@@ -48,7 +30,7 @@ describe('Server', () => {
     });
 
     it('/auth/cookies should return cookies sent on request', (done) => {
-      const agent = request.agent(server);
+      const agent = request.agent(devServer);
 
       agent
         .post('/auth/login')
