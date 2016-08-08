875c1fdfce12fb3232e8090705895931def30e22
Rupal Khilari <rupal.khilari@autodesk.com>
2016-08-08 16:45:45 -0700

    Added state persistence. Zip ape files.

--- src/containers/GlobalNav.js
+++ src/containers/GlobalNav.js
@@ -609,6 +609,14 @@ class GlobalNav extends Component {
                 this.props.inventorySelectTab('role');
               },
             },
+            {
+              text: 'GSL Library',
+              shortcut: stringToShortcut('shift meta G'),
+              action: () => {
+                this.props.inventoryToggleVisibility(true);
+                this.props.inventorySelectTab('gsl');
+              },
+            },
           ],
         },
         {
