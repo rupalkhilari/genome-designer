6d8dfdd2115ba40b24ad13e64ab162455f19711c
Maxwell Bates <max.bates@autodesk.com>
2016-08-17 22:00:26 -0700

    clean up register/update handler

--- server/auth/setUserConfigHandler.js
+++ server/auth/setUserConfigHandler.js
@@ -19,8 +19,6 @@ import { INTERNAL_HOST, API_END_POINT } from '../urlConstants';
 import { pruneUserObject, updateUserConfig } from './utils';
 import { headersPost } from '../../src/middleware/headers';
 
-//todo - need to support without auth running (local dev)
-
 //parameterized route handler for setting user config
 //expects req.user and req.config to be set
 export default function setUserConfigHandler({ useRegister = false }) {
@@ -40,36 +38,9 @@ export default function setUserConfigHandler({ useRegister = false }) {
 
     const postOptions = headersPost(JSON.stringify(user));
 
-    if (process.env.BIO_NANO_AUTH) {
-      //todo - handle them already being registered (either with GC or with auth platform)
-      if (useRegister) {
-        return fetch(url, postOptions)
-          .then(resp => {
-            //re-assign cookies from platform authentication
-            const cookies = resp.headers.getAll('set-cookie');
-            cookies.forEach(cookie => {
-              res.set('set-cookie', cookie);
-            });
-
-            return resp.json();
-          })
-          .then(userPayload => {
-            if (!!userPayload.message) {
-              return Promise.reject(userPayload);
-            }
-
-            const pruned = pruneUserObject(userPayload);
-            res.json(pruned);
-          })
-          .catch(err => {
-            console.log('got error');
-            console.log(err);
-            res.status(500).json(err);
-          });
-      }
-
-      //otherwise, update user using function
-      //todo - abstract into shared function
+    //to update user, issues with setting cookies as auth and making a new fetch, so call user update function
+    //might want to abstract to same across local + real auth
+    if (process.env.BIO_NANO_AUTH && !useRegister) {
       const userPromises = require('bio-user-platform').userPromises({
         apiEndPoint: API_END_POINT,
       });
@@ -86,7 +57,11 @@ export default function setUserConfigHandler({ useRegister = false }) {
         });
     }
 
-    //local version - just use routes
+    //todo - handle them already being registered (either with GC or with auth platform)
+
+    // otherwise, delegate to auth routes
+    // Real auth - dont need to worry about passing cookies on fetch, since registering (not authenticated)
+    // local auth - just call our mock routes
     return fetch(url, postOptions)
       .then(resp => {
         //re-assign cookies from platform authentication
